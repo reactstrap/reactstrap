f391d3f5c26b47a27a24cb4e99e8a33458f332ef
Eddy Hernandez <edward.d.hernandez@gmail.com>
2016-03-24 09:19:01 -0700

    docs(style): update social icons, logo

--- docs/lib/UI/Footer.jsx
+++ docs/lib/UI/Footer.jsx
@@ -6,18 +6,14 @@ export default () => {
       <div className="container-fluid">
         <div className="row">
           <div className="col-xs-12 text-xs-center">
-            <p>
-              <a className="m-r-1" href="https://github.com/reactstrap/reactstrap">
-                <img src="https://img.shields.io/github/stars/reactstrap/reactstrap.svg?style=social&amp;label=Star" alt="GitHub stars"/>
-              </a>
-              <a className="m-r-1" href="https://github.com/reactstrap/reactstrap">
-                <img src="https://img.shields.io/github/forks/reactstrap/reactstrap.svg?style=social&label=Fork" alt="GitHub forks"/>
-              </a>
+            <p className="social">
+              <iframe src="https://ghbtns.com/github-btn.html?user=reactstrap&repo=reactstrap&type=star&count=true" frameBorder="0" scrolling="0" width="100" height="20px"/>
+              <iframe src="https://ghbtns.com/github-btn.html?user=reactstrap&repo=reactstrap&type=fork&count=true" frameBorder="0" scrolling="0" width="100" height="20px"/>
               <a href="https://twitter.com/home?status=Easy%20to%20use%20React%20Bootstrap%204%20components%20https%3A//github.com/reactstrap/reactstrap">
                 <img src="https://img.shields.io/twitter/url/https/github.com/reactstrap/reactstrap.svg?style=social" alt="Tweet"/>
               </a>
             </p>
-          </div>
+        </div>
         </div>
       </div>
     </div>
