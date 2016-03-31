6765c98ca5f504f95fbe80dc61a0a61fa3b32a43
Eddy Hernandez <edward.d.hernandez@gmail.com>
2016-03-31 02:49:20 -0700

    refactor(Fade): use transition group

--- src/Fade.jsx
+++ src/Fade.jsx
@@ -2,53 +2,115 @@ import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 
 const propTypes = {
-  className: PropTypes.string
+  baseClass: PropTypes.string,
+  baseClassIn: PropTypes.string,
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  className: PropTypes.string,
+  transitionAppearTimeout: PropTypes.number,
+  transitionEnterTimeout: PropTypes.number,
+  transitionLeaveTimeout: PropTypes.number,
+  transitionAppear: PropTypes.bool,
+  transitionEnter: PropTypes.bool,
+  transitionLeave: PropTypes.bool,
+  onLeave: PropTypes.func
 };
 
-const defaultProps = {};
+const defaultProps = {
+  tag: 'div',
+  baseClass: 'fade',
+  baseClassIn: 'in',
+  transitionAppearTimeout: 0,
+  transitionEnterTimeout: 0,
+  transitionLeaveTimeout: 0,
+  transitionAppear: true,
+  transitionEnter: true,
+  transitionLeave: true
+};
 
 class Fade extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      fadeIn: false
+      mounted: !props.transitionAppear,
     };
-
-    this.fade = this.fade.bind(this);
-    this.fadeIn = this.fadeIn.bind(this);
-    this.fadeOut = this.fadeOut.bind(this);
   }
 
-  fade(fade, cb, delay) {
+  onEnter(cb) {
+    return () => {
+      cb();
+      if (this.props.onEnter) {
+        this.props.onEnter();
+      }
+    };
+  }
+
+  onLeave(cb) {
+    return () => {
+      cb();
+      if (this.props.onLeave) {
+        this.props.onLeave();
+      }
+    };
+  }
+
+  componentWillAppear(cb) {
+    if (!this.props.transitionAppear) {
+      this.onEnter(cb)();
+    }
+
+    setTimeout(this.onEnter(cb), this.props.transitionAppearTimeout);
+  }
+
+  componentDidAppear() {
     this.setState({
-      fadeIn: fade
+      mounted: true
+    });
+  }
+
+  componentWillEnter(cb) {
+    if (!this.props.transitionEnter) {
+      this.onEnter(cb)();
+    }
+
+    setTimeout(this.onEnter(cb), this.props.transitionEnterTimeout);
+  }
+
+  componentDidEnter() {
+    this.setState({
+      mounted: true
+    });
+  }
+
+  componentWillLeave(cb) {
+    this.setState({
+      mounted: false
     });
 
-    if (cb) {
-      setTimeout(cb, delay);
+    if (!this.props.transitionLeave) {
+      this.onLeave(cb)();
     }
-  }
 
-  fadeIn(cb, delay) {
-    this.fade(true, cb, delay);
-  }
-
-  fadeOut(cb, delay) {
-    this.fade(false, cb, delay);
+    setTimeout(this.onLeave(cb), this.props.transitionLeaveTimeout);
   }
 
   render() {
+    let {
+      baseClass,
+      baseClassIn,
+      className,
+      tag: Tag,
+      ...attributes
+    } = this.props;
+
     const classes = classNames(
-      this.props.className,
-      'fade',
-      {
-        in: this.state.fadeIn
-      }
+      className,
+      baseClass,
+      this.state.mounted ? baseClassIn : false
     );
 
     return (
-      <div {...this.props} className={classes} />
+      <Tag {...attributes} className={classes} />
     );
   }
 }
