fd1f66e21a2b513db6f570a36ebac4c3a25c5f48
Eddy Hernandez <edward.d.hernandez@gmail.com>
2016-04-08 09:19:07 -0700

    tests(enzyme): update externals

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -50,5 +50,6 @@ webpackConfig.webpackServer = {
 webpackConfig.externals = {};
 webpackConfig.externals['react/lib/ExecutionEnvironment'] = true;
 webpackConfig.externals['react/lib/ReactContext'] = true;
+webpackConfig.externals['react/addons'] = true;
 
 module.exports = webpackConfig;
