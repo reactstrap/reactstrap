14bd435c52152769041999682c1ab374946facb6
Eddy Hernandez <edward.d.hernandez@gmail.com>
2016-04-23 21:43:14 -0700

    docs(DropdownToggle): remove isOpen/toggle from props (#31)

--- docs/lib/Components/DropdownsPage.jsx
+++ docs/lib/Components/DropdownsPage.jsx
@@ -56,10 +56,8 @@ DropdownToggle.propTypes = {
   caret: PropTypes.bool,
   color: PropTypes.string,
   disabled: PropTypes.bool,
-  isOpen: PropTypes.bool,
   onClick: PropTypes.func,
-  toggle: PropTypes.func,
-  dataToggle: PropTypes.string,
+  'data-toggle': PropTypes.string,
   'aria-haspopup': PropTypes.bool
 };`}
           </PrismCode>
