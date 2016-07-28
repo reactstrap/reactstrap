96fe7083b6a2f92528416684d467af9aa7426d07
Evan Sharp <evan.sharp@availity.com>
2016-07-28 10:23:43 -0400

    Add card outline example/docs
    alpha 3 added card outline variants

---
+++ docs/lib/examples/CardOutline.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Card, Button, CardTitle, CardText } from 'reactstrap';
+
+const Example = (props) => {
+  return (
+    <div>
+      <Card block color="outline-secondary">
+        <CardTitle>Special Title Treatment</CardTitle>
+        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+        <Button>Button</Button>
+      </Card>
+      <Card block color="outline-primary">
+        <CardTitle>Special Title Treatment</CardTitle>
+        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+        <Button color="secondary">Button</Button>
+      </Card>
+      <Card block color="outline-success">
+        <CardTitle>Special Title Treatment</CardTitle>
+        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+        <Button color="secondary">Button</Button>
+      </Card>
+      <Card block color="outline-info">
+        <CardTitle>Special Title Treatment</CardTitle>
+        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+        <Button color="secondary">Button</Button>
+      </Card>
+      <Card block color="outline-warning">
+        <CardTitle>Special Title Treatment</CardTitle>
+        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+        <Button color="secondary">Button</Button>
+      </Card>
+      <Card block color="outline-danger">
+        <CardTitle>Special Title Treatment</CardTitle>
+        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+        <Button color="secondary">Button</Button>
+      </Card>
+    </div>
+  );
+};
+
+export default Example;
