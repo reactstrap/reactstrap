b307f7e995d37f090613821d273ee3bf0cfad96b
Evan Sharp <evan.sharp@availity.com>
2016-08-01 11:33:57 -0400

    feat(breadcrumb): Add Breadcrumb and BreadcrumbItem

---
+++ docs/lib/Components/BreadcrumbsPage.jsx
@@ -0,0 +1,40 @@
+/* eslint react/no-multi-comp: 0, react/prop-types: 0 */
+import React from 'react';
+import { PrismCode } from 'react-prism';
+import Helmet from 'react-helmet';
+
+import BreadcrumbExample from '../examples/Breadcrumb';
+const BreadcrumbExampleSource = require('!!raw!../examples/Breadcrumb.jsx');
+
+import BreadcrumbNoListExample from '../examples/BreadcrumbNoList';
+const BreadcrumbNoListExampleSource = require('!!raw!../examples/BreadcrumbNoList.jsx');
+
+export default class BreadcrumbsPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Helmet title="Breadcrumbs" />
+        <h3>Breadcrumbs</h3>
+        <div className="docs-example">
+          <BreadcrumbExample />
+        </div>
+        <pre>
+          <PrismCode className="language-jsx">
+            {BreadcrumbExampleSource}
+          </PrismCode>
+        </pre>
+        <h3>No list</h3>
+        <hr />
+        <p>Breadcrumbs can work without the usage of list markup.</p>
+        <div className="docs-example">
+          <BreadcrumbNoListExample />
+        </div>
+        <pre>
+          <PrismCode className="language-jsx">
+            {BreadcrumbNoListExampleSource}
+          </PrismCode>
+        </pre>
+      </div>
+    );
+  }
+}
---
+++ src/Breadcrumb.jsx
@@ -0,0 +1,33 @@
+import React, { PropTypes } from 'react';
+import classNames from 'classnames';
+
+const propTypes = {
+  tag: PropTypes.string,
+
+  className: PropTypes.any
+};
+
+const defaultProps = {
+  tag: 'ol'
+};
+
+const Breadcrumb = (props) => {
+  const {
+    className,
+    tag: Tag,
+    ...attributes
+  } = props;
+  const classes = classNames(
+    className,
+    'breadcrumb'
+  );
+
+  return (
+    <Tag {...attributes} className={classes} />
+  );
+};
+
+Breadcrumb.propTypes = propTypes;
+Breadcrumb.defaultProps = defaultProps;
+
+export default Breadcrumb;
