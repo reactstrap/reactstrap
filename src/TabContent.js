59fc841cae215b3f3571770aa14b60646423123e
Ajai <ajainarayanan@gmail.com>
2016-09-10 23:07:59 -0700

    Fixes TabContent and TabPane to use contextTypes for communicating active Tab

--- src/TabContent.js
+++ src/TabContent.js
@@ -1,9 +1,14 @@
 import React, { PropTypes, Component } from 'react';
+import classnames from 'classnames';
 
 const propTypes = {
   children: PropTypes.node,
   activeTab: PropTypes.any,
   className: PropTypes.string
+};
+
+const childContextTypes = {
+  activeTabId: PropTypes.any
 };
 
 export default class TabContent extends Component {
@@ -14,66 +19,23 @@ export default class TabContent extends Component {
       activeTab: this.props.activeTab
     };
   }
-  componentDidMount() {
-    let childRefs = Object.keys(this.childRefs);
-    if (!childRefs.length) {
-      return;
-    }
-    childRefs.forEach(childRef => {
-      this.childRefs[childRef].setAttribute('hidden', true);
-    });
-    if (typeof this.state.activeTab === 'undefined') {
-      childRefs[0].removeAttribute('hidden');
-    } else {
-      this.childRefs[this.state.activeTab].removeAttribute('hidden');
-    }
-    /*
-      FIXME: Avoid hiding and showing the parent container for all tabs.
-
-      Initially when the tab component is loaded in UI there is a brief
-      amount of time where all the tabs are visible after which componentDidMount
-      is triggered which hides non-active tabs. In order to avoid that we are hiding
-      the entire container and showing it when we decide on active tab.
-    */
-    this.containerElement.removeAttribute('hidden');
+  getChildContext() {
+    return {
+      activeTabId: this.state.activeTab
+    };
   }
   componentWillReceiveProps(nextProps) {
     this.setState({
       activeTab: nextProps.activeTab
     });
   }
-  componentDidUpdate() {
-    let childRefs = Object.keys(this.childRefs);
-    if (!childRefs.length) {
-      return;
-    }
-    let clickedChild = childRefs.filter(childRef => childRef === this.state.activeTab);
-    childRefs.forEach(ref => {
-      this.childRefs[ref].setAttribute('hidden', true);
-    });
-    this.childRefs[clickedChild[0]].removeAttribute('hidden');
-  }
   render() {
     return (
-      <div
-        className={this.props.className}
-        ref={(containerElement) => { this.containerElement = containerElement; }}
-        hidden
-      >
-        {
-          React.Children.map(this.props.children, (child) => {
-            return (
-              <div
-                className="tab-content-wrapper"
-                ref={(ref) => { this.childRefs[child.props.tabId] = ref; }}
-              >
-                {child}
-              </div>
-            );
-          }, this)
-        }
+      <div className={classnames('tab-content', this.props.className)}>
+        { this.props.children }
       </div>
     );
   }
 }
 TabContent.propTypes = propTypes;
+TabContent.childContextTypes = childContextTypes;
--- src/TabPane.js
+++ src/TabPane.js
@@ -1,16 +1,22 @@
 
 import React, { PropTypes } from 'react';
+import classnames from 'classnames';
 
 const propTypes = {
   children: PropTypes.node,
-  className: PropTypes.string
+  className: PropTypes.string,
+  tabId: PropTypes.any
+};
+const contextTypes = {
+  activeTabId: PropTypes.any
 };
 
-export default function TabPane({ children, className }) {
+export default function TabPane({ className, tabId, children }, context) {
   return (
-    <div className={className}>
+    <div className={classnames('tab-pane', className, { active: tabId === context.activeTabId })}>
       {children}
     </div>
   );
 }
 TabPane.propTypes = propTypes;
+TabPane.contextTypes = contextTypes;
