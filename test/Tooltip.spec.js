26e8bf38781ddbaa4ce2701d3fc5e7a629279ac6
Ali Sheehan-Dare <alisd23@live.co.uk>
2016-09-14 23:42:55 +0100

    feat: add delay prop to Tooltip
    Previous delay was harcoded value of 250ms for the 'hide', and 0 for the
    'show'.
    New prop 'delay' allows for either an object of form: { show: 100, hide:
    200 } or simply a number to set these delays.
    Default is { show: 0, hide: 250 }
    
    Closes #115

--- test/Tooltip.spec.js
+++ test/Tooltip.spec.js
@@ -135,9 +135,9 @@ describe('Tooltip', () => {
     wrapper.detach();
   });
 
-  it('should clear timeout if it exists on target click', () => {
+  it('should clear hide timeout if it exists on target click', () => {
     const wrapper = mount(
-      <Tooltip target="target" isOpen={isOpen} toggle={toggle}>
+        <Tooltip target="target" isOpen={isOpen} toggle={toggle} delay={200}>
         Tooltip Content
       </Tooltip>,
       { attachTo: container }
@@ -147,7 +147,7 @@ describe('Tooltip', () => {
     instance.onMouseLeaveTooltip();
     expect(isOpen).toBe(false);
     instance.handleDocumentClick({ target: target });
-    jasmine.clock().tick(250);
+    jasmine.clock().tick(200);
     expect(isOpen).toBe(true);
     wrapper.setProps({ isOpen: isOpen });
     instance.handleDocumentClick({ target: target });
@@ -176,7 +176,7 @@ describe('Tooltip', () => {
     wrapper.detach();
   });
 
-  describe('onTimeout', () => {
+  describe('hide', () => {
     it('should call toggle when isOpen', () => {
       spyOn(Tooltip.prototype, 'toggle').and.callThrough();
       isOpen = true;
@@ -190,7 +190,7 @@ describe('Tooltip', () => {
 
       expect(Tooltip.prototype.toggle).not.toHaveBeenCalled();
 
-      instance.onTimeout();
+      instance.hide();
 
       expect(Tooltip.prototype.toggle).toHaveBeenCalled();
 
@@ -209,7 +209,48 @@ describe('Tooltip', () => {
 
       expect(Tooltip.prototype.toggle).not.toHaveBeenCalled();
 
-      instance.onTimeout();
+      instance.hide();
+
+      expect(Tooltip.prototype.toggle).not.toHaveBeenCalled();
+
+      wrapper.detach();
+    });
+  });
+
+  describe('show', () => {
+    it('should call toggle when isOpen is false', () => {
+      spyOn(Tooltip.prototype, 'toggle').and.callThrough();
+      const wrapper = mount(
+        (
+          <Tooltip target="target" isOpen={isOpen} toggle={toggle}>
+            Tooltip Content
+          </Tooltip>
+      ), { attachTo: container });
+      const instance = wrapper.instance();
+
+      expect(Tooltip.prototype.toggle).not.toHaveBeenCalled();
+
+      instance.show();
+
+      expect(Tooltip.prototype.toggle).toHaveBeenCalled();
+
+      wrapper.detach();
+    });
+
+    it('should not call toggle when isOpen', () => {
+      spyOn(Tooltip.prototype, 'toggle').and.callThrough();
+      isOpen = true;
+      const wrapper = mount(
+        (
+          <Tooltip target="target" isOpen={isOpen} toggle={toggle}>
+            Tooltip Content
+          </Tooltip>
+      ), { attachTo: container });
+      const instance = wrapper.instance();
+
+      expect(Tooltip.prototype.toggle).not.toHaveBeenCalled();
+
+      instance.show();
 
       expect(Tooltip.prototype.toggle).not.toHaveBeenCalled();
 
@@ -221,7 +262,7 @@ describe('Tooltip', () => {
     it('should clear timeout if it exists on target click', () => {
       spyOn(Tooltip.prototype, 'toggle').and.callThrough();
       const wrapper = mount(
-        <Tooltip target="target" isOpen={isOpen} toggle={toggle}>
+          <Tooltip target="target" isOpen={isOpen} toggle={toggle} delay={{show: 200, hide: 200}}>
           Tooltip Content
         </Tooltip>,
         { attachTo: container }
@@ -234,6 +275,7 @@ describe('Tooltip', () => {
       expect(Tooltip.prototype.toggle).not.toHaveBeenCalled();
 
       instance.onMouseOverTooltip();
+      jasmine.clock().tick(200);
 
       expect(Tooltip.prototype.toggle).toHaveBeenCalled();
 
@@ -244,7 +286,29 @@ describe('Tooltip', () => {
       spyOn(Tooltip.prototype, 'toggle').and.callThrough();
       isOpen = true;
       const wrapper = mount(
-        <Tooltip target="target" isOpen={isOpen} toggle={toggle}>
+          <Tooltip target="target" isOpen={isOpen} toggle={toggle} delay={0}>
+            Tooltip Content
+          </Tooltip>
+      ), { attachTo: container });
+      const instance = wrapper.instance();
+
+      instance.onMouseOverTooltip();
+      jasmine.clock().tick(0);  // delay: 0 toggle is still async
+
+      expect(isOpen).toBe(true);
+      expect(Tooltip.prototype.toggle).not.toHaveBeenCalled();
+
+      wrapper.detach();
+    });
+  });
+
+  describe('onMouseLeaveTooltip', () => {
+    it('should clear timeout if it exists on target click', () => {
+      spyOn(Tooltip.prototype, 'toggle').and.callThrough();
+      isOpen = true;
+      const wrapper = mount(
+        (
+          <Tooltip target="target" isOpen={isOpen} toggle={toggle} delay={{show: 200, hide: 200}}>
           Tooltip Content
         </Tooltip>,
         { attachTo: container }
@@ -254,6 +318,31 @@ describe('Tooltip', () => {
       instance.onMouseOverTooltip();
 
       expect(isOpen).toBe(true);
+      expect(Tooltip.prototype.toggle).not.toHaveBeenCalled();
+
+      instance.onMouseLeaveTooltip();
+      jasmine.clock().tick(200);
+
+      expect(Tooltip.prototype.toggle).toHaveBeenCalled();
+
+      wrapper.detach();
+    });
+
+    it('should not call .toggle if isOpen is false', () => {
+      spyOn(Tooltip.prototype, 'toggle').and.callThrough();
+      isOpen = false;
+      const wrapper = mount(
+        (
+          <Tooltip target="target" isOpen={isOpen} toggle={toggle} delay={0}>
+            Tooltip Content
+          </Tooltip>
+      ), { attachTo: container });
+      const instance = wrapper.instance();
+
+      instance.onMouseLeaveTooltip();
+      jasmine.clock().tick(0);  // delay: 0 toggle is still async
+
+      expect(isOpen).toBe(false);
       expect(Tooltip.prototype.toggle).not.toHaveBeenCalled();
 
       wrapper.detach();
