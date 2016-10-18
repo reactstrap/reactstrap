8eb561f217c792f551071f9d20b04e68dee60533
Evan Sharp <evan.sharp@availity.com>
2016-10-18 15:32:26 -0400

    chore(docs): button spacing
    This removes the special button margin which is in the docs and
    replaces it with `{' '}` to add an actual space in the HTML markup.
    This makes the button spacing work like bootstrap does and documents
    `{' '}` being used.

--- docs/lib/examples/ButtonOutline.js
+++ docs/lib/examples/ButtonOutline.js
@@ -5,11 +5,11 @@ export default class Example extends React.Component {
   render() {
     return (
       <div>
-        <Button outline color="primary">primary</Button>
-        <Button outline color="secondary">secondary</Button>
-        <Button outline color="success">success</Button>
-        <Button outline color="info">info</Button>
-        <Button outline color="warning">warning</Button>
+        <Button outline color="primary">primary</Button>{' '}
+        <Button outline color="secondary">secondary</Button>{' '}
+        <Button outline color="success">success</Button>{' '}
+        <Button outline color="info">info</Button>{' '}
+        <Button outline color="warning">warning</Button>{' '}
         <Button outline color="danger">danger</Button>
       </div>
     );
