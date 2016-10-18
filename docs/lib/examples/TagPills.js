fbaba3d1d243b7f2060de41c035b7a601ef1696a
Evan Sharp <dumbdrum@gmail.com>
2016-10-18 15:55:38 -0400

    docs(button): switch to empty string for spacing (#186)
    This removes the special button margin which is in the docs and
    replaces it with `{' '}` to add an actual space in the HTML markup.
    This makes the button spacing work like bootstrap does and documents
    `{' '}` being used.

--- docs/lib/examples/TagPills.js
+++ docs/lib/examples/TagPills.js
@@ -5,11 +5,11 @@ export default class Example extends React.Component {
   render() {
     return (
       <div>
-        <Tag color="default" pill>default</Tag>
-        <Tag color="primary" pill>primary</Tag>
-        <Tag color="success" pill>success</Tag>
-        <Tag color="info" pill>info</Tag>
-        <Tag color="warning" pill>warning</Tag>
+        <Tag color="default" pill>default</Tag>{' '}
+        <Tag color="primary" pill>primary</Tag>{' '}
+        <Tag color="success" pill>success</Tag>{' '}
+        <Tag color="info" pill>info</Tag>{' '}
+        <Tag color="warning" pill>warning</Tag>{' '}
         <Tag color="danger" pill>danger</Tag>
       </div>
     );
