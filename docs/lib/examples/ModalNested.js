9cdff49cfd77251259605848bb09369ca61968ad
Evan Sharp <evan.sharp@availity.com>
2016-10-19 09:15:13 -0400

    chore(docs): Fix modal button spacing

--- docs/lib/examples/ModalNested.js
+++ docs/lib/examples/ModalNested.js
@@ -35,21 +35,19 @@ class ModalExample extends React.Component {
           <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
           <ModalBody>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            <br/>
+            <br />
             <Button color="success" onClick={this.toggleNested}>Show Nested Model</Button>
             <Modal isOpen={this.state.nestedModal} toggle={this.toggleNested}>
               <ModalHeader>Nested Modal title</ModalHeader>
               <ModalBody>Stuff and things</ModalBody>
               <ModalFooter>
-                <Button color="primary" onClick={this.toggleNested}>Done</Button>
-                &nbsp;
+                <Button color="primary" onClick={this.toggleNested}>Done</Button>{' '}
                 <Button color="secondary" onClick={this.toggle}>All Done</Button>
               </ModalFooter>
             </Modal>
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Do Something</Button>
-            &nbsp;
+            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
             <Button color="secondary" onClick={this.toggle}>Cancel</Button>
           </ModalFooter>
         </Modal>
