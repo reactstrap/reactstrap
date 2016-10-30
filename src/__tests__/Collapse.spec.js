2c7ee498326172c4c8f3ab2e0b8d40b5433e7075
LiJinyao <lijinyao@outlook.com>
2016-10-30 13:13:43 +0800

    add more test

--- src/__tests__/Collapse.spec.js
+++ src/__tests__/Collapse.spec.js
@@ -3,6 +3,21 @@ import { shallow, mount } from 'enzyme';
 import Collapse from '../Collapse';
 
 describe('Collapse', () => {
+  let isOpen;
+  let toggle;
+
+  beforeEach(() => {
+    isOpen = false;
+    toggle = () => { isOpen = !isOpen; };
+    jasmine.clock().install();
+  });
+
+  afterEach(() => {
+    // fast forward time for collapse to fade out
+    jasmine.clock().tick(400);
+    jasmine.clock().uninstall();
+  });
+
   it('should render children', () => {
     const wrapper = shallow(<Collapse><p>hello</p></Collapse>).find('p');
     expect(wrapper.text()).toBe('hello');
@@ -13,19 +28,56 @@ describe('Collapse', () => {
     expect(wrapper.instance().props.isOpen).toEqual(false);
   });
 
-  it('should render with calss "collapse"', () => {
+  it('should render with class "collapse"', () => {
     const wrapper = shallow(<Collapse />);
     expect(wrapper.hasClass('collapse')).toEqual(true);
   });
 
-  it('should render with calss "in" when isOpen is true', () => {
+  it('should render with class "in" when isOpen is true', () => {
     const wrapper = shallow(<Collapse isOpen />);
     expect(wrapper.hasClass('in')).toEqual(true);
   });
 
-  it('should change state with { collapse: ${State} } when isOpen change to false', () => {
-    const wrapper = mount(<Collapse isOpen />);
-    wrapper.setProps({ isOpen: false });
+  it('should change state with { collapse: ${State} } when isOpen change to true before transition', () => {
+    const wrapper = mount(<Collapse isOpen={isOpen} />);
+    toggle();
+    wrapper.setProps({ isOpen: isOpen });
+    expect(wrapper.state('collapse')).toEqual('SHOW');
+    wrapper.unmount();
+  });
+
+  it('should change state with { collapse: ${State} } when isOpen change to true after transition', () => {
+    const wrapper = mount(<Collapse isOpen={isOpen} />);
+    toggle();
+    wrapper.setProps({ isOpen: isOpen });
+    jasmine.clock().tick(350);
+    expect(wrapper.state('collapse')).toEqual('SHOWN');
+    wrapper.unmount();
+  });
+
+  it('should change state with { collapse: ${State} } when isOpen change to false before transition', () => {
+    isOpen = true;
+    const wrapper = mount(<Collapse isOpen={isOpen} />);
+    toggle();
+    wrapper.setProps({ isOpen: isOpen });
     expect(wrapper.state('collapse')).toEqual('HIDE');
+    wrapper.unmount();
+  });
+
+  it('should change state with { collapse: ${State} } when isOpen change to false after transition', () => {
+    isOpen = true;
+    const wrapper = mount(<Collapse isOpen={isOpen} />);
+    toggle();
+    wrapper.setProps({ isOpen: isOpen });
+    jasmine.clock().tick(360);
+    expect(wrapper.state('collapse')).toEqual('HIDDEN');
+    wrapper.unmount();
+  });
+
+  it('should remove timeout tag after unmount', () => {
+    spyOn(Collapse.prototype, 'componentWillUnmount').and.callThrough();
+    const wrapper = mount(<Collapse isOpen={isOpen} />);
+    wrapper.unmount();
+    expect(Collapse.prototype.componentWillUnmount).toHaveBeenCalled();
   });
 });
