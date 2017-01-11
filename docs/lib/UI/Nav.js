e973682026de63195664884b9b674edc98d398a6
Eddy Hernandez <edward.d.hernandez@gmail.com>
2017-01-10 20:19:26 -0800

    docs(Nav): update navs

--- docs/lib/UI/Nav.js
+++ docs/lib/UI/Nav.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router';
-import { Container, Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { NavbarToggler, Container, Collapse, Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
-export default () => {
-  return (
-    <Navbar className="header" full color="faded" light>
-      <Container>
-        <NavbarBrand className="mr-auto" tag={Link} to="/">reactstrap</NavbarBrand>
-        <Nav navbar className="ml-sm-auto">
-          <NavItem>
-            <NavLink tag={Link} className="nav-link" to="/components/" activeClassName="active">Components</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
-          </NavItem>
-        </Nav>
-      </Container>
-    </Navbar>
-  );
-};
+
+export default class UINav extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      showNavbar: false
+    };
+  }
+  toggleNavbar(e) {
+    e.preventDefault();
+    this.setState({
+      showNavbar: !this.state.showNavbar
+    });
+  }
+  render() {
+    return (
+      <Navbar className="header" full color="faded" light toggleable>
+        <Container>
+          <NavbarToggler right onClick={this.toggleNavbar} />
+          <NavbarBrand className="mr-auto" tag={Link} to="/">reactstrap</NavbarBrand>
+          <Collapse navbar isOpen={this.state.showNavbar}>
+            <Nav navbar className="ml-sm-auto">
+              <NavItem>
+                <NavLink tag={Link} className="nav-link" to="/components/" activeClassName="active">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}
--- docs/lib/examples/Navbar.js
+++ docs/lib/examples/Navbar.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
     return (
       <div>
-        <Navbar color="faded" light>
+        <Navbar color="faded" light toggleable>
+          <NavbarToggler right onClick={this.toggle} />
           <NavbarBrand href="/">reactstrap</NavbarBrand>
-          <Nav className="float-right" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
-            </NavItem>
-          </Nav>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
         </Navbar>
       </div>
     );
