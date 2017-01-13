063949215893d86fc2db002b0860a7d51c3fbe37
Eddy Hernandez <edward.d.hernandez@gmail.com>
2017-01-12 22:02:14 -0800

    docs(CustomDropdown): add custom dropdown docs

---
+++ docs/lib/examples/CustomDropdown.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Dropdown, DropdownMenu } from 'reactstrap';
+
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
+  render() {
+    return (
+      <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+        <span
+          onClick={this.toggle}
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded={this.state.dropdownOpen}
+        >
+          Custom Dropdown Content
+        </span>
+        <DropdownMenu>
+          <div onClick={this.toggle}>Custom dropdown item</div>
+          <div onClick={this.toggle}>Custom dropdown item</div>
+          <div onClick={this.toggle}>Custom dropdown item</div>
+          <div onClick={this.toggle}>Custom dropdown item</div>
+        </DropdownMenu>
+      </Dropdown>
+    );
+  }
+}
