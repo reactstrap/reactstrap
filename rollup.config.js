64f1f687b8082485b2cabda1250b75f143a9e982
Paulus Schoutsen <paulus.schoutsen@mycase.com>
2017-03-30 13:21:36 -0700

    Remove unused options from rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,7 @@ import babel from 'rollup-plugin-babel';
 import babili from 'rollup-plugin-babili';
 
 const config = {
-  moduleName: 'ReactStrap',
   entry: 'src/index.js',
-  exports: 'named',
   plugins: [
     nodeResolve(),
     commonjs(),
@@ -20,13 +18,6 @@ const config = {
     'react-addons-css-transition-group',
     'react-addons-transition-group',
   ],
-  // Used for the UMD bundles
-  globals: {
-    react: 'React',
-    'react-dom': 'ReactDOM',
-    'react-addons-css-transition-group': 'CSSTransitionGroup',
-    'react-addons-transition-group': 'TransitionGroup',
-  },
   targets: [
     { dest: 'dist/reactstrap.cjs.js', format: 'cjs' },
     { dest: 'dist/reactstrap.es.js', format: 'es' },
