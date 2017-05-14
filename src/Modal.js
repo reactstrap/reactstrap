a4fec3c52e0b0e4b5704c64440dc44652a5f479e
Sebastian Roth <sebastian.roth@gmail.com>
2017-05-15 07:03:09 +0800

    fix(react-addons): move to external react-transition-group dependency (#399)

--- src/Modal.js
+++ src/Modal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import classNames from 'classnames';
-import TransitionGroup from 'react-addons-transition-group';
+import TransitionGroup from 'react-transition-group/TransitionGroup';
 import Fade from './Fade';
 import {
   getOriginalBodyPadding,
