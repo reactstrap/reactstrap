2c6b35d570c37883991b6a75ea3bf751bd60a64d
Paulus Schoutsen <paulus.schoutsen@mycase.com>
2017-06-23 10:03:15 -0700

    Update transition group imports

--- rollup.config.js
+++ rollup.config.js
@@ -19,8 +19,6 @@ const config = {
     'react',
     'react-dom',
     'react-transition-group',
-    'react-transition-group/CSSTransitionGroup',
-    'react-transition-group/TransitionGroup'
   ],
   // Used for the UMD bundles
   globals: {
