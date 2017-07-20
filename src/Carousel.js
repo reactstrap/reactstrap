e1bdadb854cde2a1480fce5ac5638b225e3c8577
Greg Thornton <xdissent@me.com>
2017-07-20 04:57:16 -0500

    fix(Carousel): use prop-types in carousel (#503)

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { TransitionGroup } from 'react-transition-group';
 import { mapToCssModules } from './utils';
