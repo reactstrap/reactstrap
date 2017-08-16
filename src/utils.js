5faa94d9796bfa0f3b247571c2257d0d9fdccd92
Evan Sharp <evan.sharp@availity.com>
2017-08-16 13:08:09 -0400

    feat(util): add warnOnce and deprecated fns
    Add deprected util to mark prop-types as deprected
    Add warnOnce to display a message in the console, never the same message twice

--- src/utils.js
+++ src/utils.js
@@ -167,3 +167,24 @@ export function omit(obj, omitKeys) {
   });
   return result;
 }
+
+let warned = {};
+
+export function warnOnce(message) {
+  if (!warned[message]) {
+    if (typeof console !== 'undefined') {
+      console.error(message); // eslint-disable-line no-console
+    }
+    warned[message] = true;
+  }
+}
+
+export function deprecated(propType, explanation) {
+  return function validate(props, propName, componentName, ...rest) {
+    if (props[propName] !== null && typeof props[propName] !== 'undefined') {
+      warnOnce(`"${propName}" property of "${componentName}" has been deprecated.\n${explanation}`);
+    }
+
+    return propType(props, propName, componentName, ...rest);
+  };
+}
