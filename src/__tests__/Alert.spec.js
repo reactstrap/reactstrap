704ae36f038fdcb91012093e58e80fd84b254b14
Alex Mikhalev <alexmikhalevalex@gmail.com>
2017-08-28 22:47:56 -0600

    Fixed Alert tests for new transition props

--- src/__tests__/Alert.spec.js
+++ src/__tests__/Alert.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { Alert } from '../';
 
 describe('Alert', () => {
@@ -9,45 +9,43 @@ describe('Alert', () => {
   });
 
   it('should transition on appear, enter, and leave using fade', () => {
-    const alert = shallow(<Alert>Yo!</Alert>);
-    expect(alert.prop('transitionName')).toEqual({
+    const alert = mount(<Alert>Yo!</Alert>);
+    expect(alert.find("CSSTransition").prop('classNames')).toEqual({
       appear: 'fade',
       appearActive: 'show',
       enter: 'fade',
       enterActive: 'show',
-      leave: 'fade',
-      leaveActive: 'out'
+      exit: 'fade',
+      exitActive: 'out'
     });
   });
 
   it('should have default transitionTimeouts', () => {
-    const alert = shallow(<Alert>Yo!</Alert>);
+    const alert = mount(<Alert>Yo!</Alert>);
 
-    expect(alert.prop('transitionAppearTimeout')).toBe(150);
-    expect(alert.prop('transitionAppear')).toBe(true);
-    expect(alert.prop('transitionEnterTimeout')).toBe(150);
-    expect(alert.prop('transitionEnter')).toBe(true);
-    expect(alert.prop('transitionLeaveTimeout')).toBe(150);
-    expect(alert.prop('transitionLeave')).toBe(true);
+    const transition = alert.find("CSSTransition");
+    expect(transition.prop('timeout')).toEqual({ enter: 150, exit: 150 });
+    expect(transition.prop('appear')).toBe(true);
+    expect(transition.prop('enter')).toBe(true);
+    expect(transition.prop('exit')).toBe(true);
   });
 
   it('should have support configurable transitionTimeouts', () => {
     const alert = shallow(
       <Alert
-        transitionAppearTimeout={0}
+        transitionAppear={false}
         transitionEnterTimeout={0}
-        transitionLeaveTimeout={0}
+        transitionExitTimeout={0}
       >
         Yo!
       </Alert>
     );
 
-    expect(alert.prop('transitionAppearTimeout')).toBe(0);
-    expect(alert.prop('transitionAppear')).toBe(false);
-    expect(alert.prop('transitionEnterTimeout')).toBe(0);
-    expect(alert.prop('transitionEnter')).toBe(false);
-    expect(alert.prop('transitionLeaveTimeout')).toBe(0);
-    expect(alert.prop('transitionLeave')).toBe(false);
+    const transition = alert.find("CSSTransition");
+    expect(transition.prop('timeout')).toEqual({ enter: 0, exit: 0 });
+    expect(transition.prop('appear')).toBe(false);
+    expect(transition.prop('enter')).toBe(false);
+    expect(transition.prop('exit')).toBe(false);
   });
 
   it('should have "success" as default color', () => {
@@ -61,7 +59,7 @@ describe('Alert', () => {
   });
 
   it('should use a div tag by default', () => {
-    const alert = shallow(<Alert>Yo!</Alert>).children().first();
+    const alert = shallow(<Alert>Yo!</Alert>).children().first().children().first();
     expect(alert.type()).toBe('div');
   });
 
@@ -72,13 +70,13 @@ describe('Alert', () => {
   });
 
   it('should show dismiss button if passed toggle', () => {
-    const alert = shallow(<Alert color="danger" toggle={() => {}}>Yo!</Alert>).find('div');
+    const alert = shallow(<Alert color="danger" toggle={() => { }}>Yo!</Alert>).find('div');
     expect(alert.find('button').length).toEqual(1);
     expect(alert.hasClass('alert-dismissible')).toBe(true);
   });
 
   it('should support custom tag', () => {
-    const alert = shallow(<Alert tag="p">Yo!</Alert>).children().first();
+    const alert = shallow(<Alert tag="p">Yo!</Alert>).children().first().children().first();
     expect(alert.type()).toBe('p');
   });
 
@@ -96,7 +94,7 @@ describe('Alert', () => {
   });
 
   it('should render close button with custom aria-label', () => {
-    const alert = shallow(<Alert toggle={() => {}} closeAriaLabel="oseclay">Yo!</Alert>).find('div');
+    const alert = shallow(<Alert toggle={() => { }} closeAriaLabel="oseclay">Yo!</Alert>).find('div');
     const closeButton = alert.find('button').first();
     expect(closeButton.prop('aria-label')).toBe('oseclay');
   });
