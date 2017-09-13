db245a744e54c294cfa05c3b32a8971280735a7b
Marek Takac <marek.takac@nejremeslnici.cz>
2017-09-13 19:49:35 +0200

    docs(Badge) update and fix docs (#538)
    Remove default badge and update documentation so it's closer to bootstrap v4 beta
    docs

--- docs/lib/examples/BadgePills.js
+++ docs/lib/examples/BadgePills.js
@@ -5,12 +5,14 @@ export default class Example extends React.Component {
   render() {
     return (
       <div>
-        <Badge color="default" pill>default</Badge>{' '}
-        <Badge color="primary" pill>primary</Badge>{' '}
-        <Badge color="success" pill>success</Badge>{' '}
-        <Badge color="info" pill>info</Badge>{' '}
-        <Badge color="warning" pill>warning</Badge>{' '}
-        <Badge color="danger" pill>danger</Badge>
+        <Badge color="primary" pill>Primary</Badge>
+        <Badge color="secondary" pill>Secondary</Badge>
+        <Badge color="success" pill>Success</Badge>
+        <Badge color="danger" pill>Danger</Badge>
+        <Badge color="warning" pill>Warning</Badge>
+        <Badge color="info" pill>Info</Badge>
+        <Badge color="light" pill>Light</Badge>
+        <Badge color="dark" pill>Dark</Badge>
       </div>
     );
   }
