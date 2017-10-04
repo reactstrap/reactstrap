3fbd34c83c7a5ad1a651efbc25a42db61479e51d
Mark Johnson <mark@techpivot.net>
2017-10-04 14:59:30 -0700

    chore(build): include unminified version in distribution (#618)
    chore(build): Include unminified version in distribution

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -3,24 +3,29 @@ var fs = require('fs');
 // Ensure that any optional peer dependency (e.g. ReactTransitionGroup and
 // ReactPopper) are properly set as optional globals. Specifically, do not
 // error if the user has not loaded these dependencies. Degrade gracefully.
+const files = [
+  __dirname + '/../dist/reactstrap.js',
+  __dirname + '/../dist/reactstrap.min.js',
+];
 
-fs.readFile(__dirname + '/../dist/reactstrap.min.js', 'utf8', function(err, data) {
-  if (err) {
-    return console.error(err);
-  }
-
-  var result = data.replace(
-    /\,([a-zA-Z]+)\.ReactTransitionGroup\.Transition/,
-    ',$1.ReactTransitionGroup && $1.ReactTransitionGroup.Transition || undefined'
-  ).replace(
-    /\,([a-zA-Z]+)\.ReactPopper/,
-    ',$1.ReactPopper || undefined'
-  );
-
-  fs.writeFile(__dirname + '/../dist/reactstrap.min.js', result, 'utf8', function(err) {
+files.forEach(function(file) {
+  fs.readFile(file, 'utf8', function(err, data) {
     if (err) {
-      console.error(err);
+      return console.error(err);
     }
-  });
 
+    var result = data.replace(
+      /\,([a-zA-Z]+)\.ReactTransitionGroup\.Transition/,
+      ',$1.ReactTransitionGroup && $1.ReactTransitionGroup.Transition || undefined'
+    ).replace(
+      /\,([a-zA-Z]+)\.ReactPopper/,
+      ',$1.ReactPopper || undefined'
+    );
+
+    fs.writeFile(file, result, 'utf8', function(err) {
+      if (err) {
+        console.error(err);
+      }
+    });
+  });
 });
