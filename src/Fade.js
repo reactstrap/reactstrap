0cdbf67fc550d0c1a65667189b9285e34ed6900c
Devon Jue <devonjue@Devons-MacBook-Pro.local>
2017-10-11 01:44:12 -0700

    #607 animation on modal mounting

--- src/Fade.js
+++ src/Fade.js
@@ -57,7 +57,7 @@ function Fade(props) {
   return (
     <Transition {...transitionProps}>
       {(status) => {
-        const isActive = status === 'entering' || status === 'entered';
+        const isActive = status === 'entered';
         const classes = mapToCssModules(classNames(
           className,
           baseClass,
