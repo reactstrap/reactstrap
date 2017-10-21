d2b61d9c9b216e5d41eaaaeb3299ce9b0c7c4082
Evan Sharp <dumbdrum@gmail.com>
2017-10-21 12:02:01 -0400

    tests passing

--- src/__tests__/Carousel.spec.js
+++ src/__tests__/Carousel.spec.js
@@ -24,8 +24,8 @@ describe('Carousel', () => {
   describe('captions', () => {
     it('should render a header and a caption', () => {
       const wrapper = mount(<CarouselCaption captionHeader="abc" captionText="def" />);
-      expect(wrapper.find('h3').hostNodes().length).toEqual(1);
-      expect(wrapper.find('p').hostNodes().length).toEqual(1);
+      expect(wrapper.find('h3').length).toEqual(1);
+      expect(wrapper.find('p').length).toEqual(1);
     });
   });
 
@@ -49,26 +49,26 @@ describe('Carousel', () => {
         const wrapper = mount(<CarouselItem src={items[0].src} altText={items[0].src} in={false} />, { context: { direction: 'right' } });
 
         wrapper.setProps({ in: true });
-        expect(wrapper.find('div').hostNodes().prop('className')).toEqual('carousel-item carousel-item-left carousel-item-next');
+        expect(wrapper.update().find('div').prop('className')).toEqual('carousel-item carousel-item-left carousel-item-next');
         jest.runTimersToTime(600);
-        expect(wrapper.find('div').hostNodes().prop('className')).toEqual('carousel-item active');
+        expect(wrapper.update().find('div').prop('className')).toEqual('carousel-item active');
         wrapper.setProps({ in: false });
-        expect(wrapper.find('div').hostNodes().prop('className')).toEqual('carousel-item active carousel-item-left');
+        expect(wrapper.update().find('div').prop('className')).toEqual('carousel-item active carousel-item-left');
         jest.runTimersToTime(600);
-        expect(wrapper.find('div').hostNodes().prop('className')).toEqual('carousel-item');
+        expect(wrapper.update().find('div').prop('className')).toEqual('carousel-item');
       });
 
       it('should add the appropriate classes when entering left', () => {
         const wrapper = mount(<CarouselItem src={items[0].src} altText={items[0].src} in={false} />, { context: { direction: 'left' } });
 
         wrapper.setProps({ in: true });
-        expect(wrapper.find('div').hostNodes().prop('className')).toEqual('carousel-item carousel-item-right carousel-item-prev');
+        expect(wrapper.update().find('div').prop('className')).toEqual('carousel-item carousel-item-right carousel-item-prev');
         jest.runTimersToTime(600);
-        expect(wrapper.find('div').hostNodes().prop('className')).toEqual('carousel-item active');
+        expect(wrapper.update().find('div').prop('className')).toEqual('carousel-item active');
         wrapper.setProps({ in: false });
-        expect(wrapper.find('div').hostNodes().prop('className')).toEqual('carousel-item active carousel-item-right');
+        expect(wrapper.update().find('div').prop('className')).toEqual('carousel-item active carousel-item-right');
         jest.runTimersToTime(600);
-        expect(wrapper.find('div').hostNodes().prop('className')).toEqual('carousel-item');
+        expect(wrapper.update().find('div').prop('className')).toEqual('carousel-item');
       });
 
       it('should call all callbacks when transitioning in and out', () => {
@@ -105,8 +105,8 @@ describe('Carousel', () => {
   describe('indicators', () => {
     it('should render a list with the right number of items', () => {
       const wrapper = mount(<CarouselIndicators items={items} activeIndex={0} onClickHandler={() => { }} />);
-      expect(wrapper.find('ol').hostNodes().length).toEqual(1);
-      expect(wrapper.find('li').hostNodes().length).toEqual(3);
+      expect(wrapper.find('ol').length).toEqual(1);
+      expect(wrapper.find('li').length).toEqual(3);
     });
 
     it('should append the correct active class', () => {
@@ -117,7 +117,7 @@ describe('Carousel', () => {
     it('should call the click hanlder', () => {
       const onClick = jest.fn();
       const wrapper = mount(<CarouselIndicators items={items} activeIndex={0} onClickHandler={onClick} />);
-      wrapper.find('li').hostNodes().first().simulate('click');
+      wrapper.find('li').first().simulate('click');
       expect(onClick).toHaveBeenCalled();
     });
   });
@@ -125,13 +125,13 @@ describe('Carousel', () => {
   describe('controls', () => {
     it('should render an anchor tag', () => {
       const wrapper = mount(<CarouselControl direction="next" onClickHandler={() => { }} />);
-      expect(wrapper.find('a').hostNodes().length).toEqual(1);
+      expect(wrapper.find('a').length).toEqual(1);
     });
 
     it('should call the onClickHandler', () => {
       const onClick = jest.fn();
       const wrapper = mount(<CarouselControl direction="next" onClickHandler={onClick} />);
-      wrapper.find('a').hostNodes().first().simulate('click');
+      wrapper.find('a').first().simulate('click');
       expect(onClick).toHaveBeenCalled();
     });
   });
