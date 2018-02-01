fda0d16d1da263a2df3b3afe99e8d575320e358d
Oleg Artenii <oleg@artenii.email>
2018-02-01 16:33:12 +0200

    removed unused parameter

--- docs/lib/examples/NavbarToggler.js
+++ docs/lib/examples/NavbarToggler.js
@@ -12,7 +12,7 @@ export default class Example extends React.Component {
   }
 
   toggleNavbar() {
-    this.setState((prevState, props) => ({
+    this.setState(prevState => ({
       collapsed: !prevState.collapsed
     }));
   }
