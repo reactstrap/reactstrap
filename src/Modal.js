083ae721b30ed5576cb828c16a9e09c9471ea92b
Evan Sharp <dumbdrum@gmail.com>
2018-02-09 12:36:28 -0500

    fix(Modal): don't add or remove multi body classes

--- src/Modal.js
+++ src/Modal.js
@@ -155,11 +155,12 @@ class Modal extends React.Component {
       this._element = null;
     }
 
-    if (!this.props.isOpen) {
+    if (this.bodyClassAdded) {
       const modalOpenClassName = mapToCssModules('modal-open', this.props.cssModule);
       // Use regex to prevent matching `modal-open` as part of a different class, e.g. `my-modal-opened`
       const modalOpenClassNameRegex = new RegExp(`(^| )${modalOpenClassName}( |$)`);
       document.body.className = document.body.className.replace(modalOpenClassNameRegex, ' ').trim();
+      this.bodyClassAdded = false;
       setScrollbarWidth(this.originalBodyPadding);
     }
   }
@@ -186,10 +187,13 @@ class Modal extends React.Component {
 
     document.body.appendChild(this._element);
 
-    document.body.className = classNames(
-      classes,
-      mapToCssModules('modal-open', this.props.cssModule)
-    );
+    if (!this.bodyClassAdded) {
+      document.body.className = classNames(
+        classes,
+        mapToCssModules('modal-open', this.props.cssModule)
+      );
+      this.bodyClassAdded = true;
+    }
 
     this.renderIntoSubtree();
   }
