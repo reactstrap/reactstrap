877c4872078602802542d5495ace1d3aaf9683df
Evan Sharp <dumbdrum@gmail.com>
2018-02-22 12:23:18 -0500

    Update Modal.spec.js

--- src/__tests__/Modal.spec.js
+++ src/__tests__/Modal.spec.js
@@ -61,7 +61,7 @@ describe('Modal', () => {
 
     jest.runTimersToTime(300);
     expect(document.getElementsByClassName('cool-close-button').length).toBe(1);
-    expect(document.getElementsByClassName('cool-close-button').nextElementSibling.className.split(' ').indexOf('modal-dialog') > -1).toBe(true);
+    expect(document.getElementsByClassName('cool-close-button')[0].nextElementSibling.className.split(' ').indexOf('modal-dialog') > -1).toBe(true);
     wrapper.unmount();
   });
 
