00c27501813e3b274514df0c74e6fc77a7b68f68
JuanMa Garrido <juanma.garrido@gmail.com>
2018-06-16 15:04:11 +0200

    fix(Tooltip): restored format

--- src/Tooltip.js
+++ src/Tooltip.js
@@ -2,22 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import PopperContent from './PopperContent';
-import {
-  getTarget,
-  DOMElement,
-  mapToCssModules,
-  omit,
-  PopperPlacements
-} from './utils';
+import { getTarget, DOMElement, mapToCssModules, omit, PopperPlacements } from './utils';
 
 const propTypes = {
   placement: PropTypes.oneOf(PopperPlacements),
-  target: PropTypes.oneOfType([PropTypes.string, PropTypes.func, DOMElement])
-    .isRequired,
+  target: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.func,
+    DOMElement,
+  ]).isRequired,
   container: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.func,
-    DOMElement
+    DOMElement,
   ]),
   isOpen: PropTypes.bool,
   disabled: PropTypes.bool,
@@ -30,10 +27,13 @@ const propTypes = {
   placementPrefix: PropTypes.string,
   delay: PropTypes.oneOfType([
     PropTypes.shape({ show: PropTypes.number, hide: PropTypes.number }),
-    PropTypes.number
+    PropTypes.number,
   ]),
   modifiers: PropTypes.object,
-  offset: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+  offset: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]),
 };
 
 const DEFAULT_DELAYS = {
@@ -48,7 +48,7 @@ const defaultProps = {
   placementPrefix: 'bs-tooltip',
   delay: DEFAULT_DELAYS,
   autohide: true,
-  toggle: function () {}
+  toggle: function () { }
 };
 
 class Tooltip extends React.Component {
@@ -62,9 +62,7 @@ class Tooltip extends React.Component {
     this.onMouseOverTooltip = this.onMouseOverTooltip.bind(this);
     this.onMouseLeaveTooltip = this.onMouseLeaveTooltip.bind(this);
     this.onMouseOverTooltipContent = this.onMouseOverTooltipContent.bind(this);
-    this.onMouseLeaveTooltipContent = this.onMouseLeaveTooltipContent.bind(
-      this
-    );
+    this.onMouseLeaveTooltipContent = this.onMouseLeaveTooltipContent.bind(this);
     this.show = this.show.bind(this);
     this.hide = this.hide.bind(this);
     this.onEscKeyDown = this.onEscKeyDown.bind(this);
@@ -169,22 +167,15 @@ class Tooltip extends React.Component {
     this._target.addEventListener('focusin', this.show, true);
     this._target.addEventListener('focusout', this.hide, true);
 
+
     ['click', 'touchstart'].forEach(event =>
       document.addEventListener(event, this.handleDocumentClick, true)
     );
   }
 
   removeTargetEvents() {
-    this._target.removeEventListener(
-      'mouseover',
-      this.onMouseOverTooltip,
-      true
-    );
-    this._target.removeEventListener(
-      'mouseout',
-      this.onMouseLeaveTooltip,
-      true
-    );
+    this._target.removeEventListener('mouseover', this.onMouseOverTooltip, true);
+    this._target.removeEventListener('mouseout', this.onMouseLeaveTooltip, true);
     this._target.addEventListener('keydown', this.onEscKeyDown, true);
     this._target.addEventListener('focusin', this.show, true);
     this._target.addEventListener('focusout', this.hide, true);
@@ -208,15 +199,16 @@ class Tooltip extends React.Component {
     }
 
     const attributes = omit(this.props, Object.keys(propTypes));
-    const classes = mapToCssModules(
-      classNames('tooltip-inner', this.props.innerClassName),
-      this.props.cssModule
-    );
+    const classes = mapToCssModules(classNames(
+      'tooltip-inner',
+      this.props.innerClassName
+    ), this.props.cssModule);
 
-    const popperClasses = mapToCssModules(
-      classNames('tooltip', 'show', this.props.className),
-      this.props.cssModule
-    );
+    const popperClasses = mapToCssModules(classNames(
+      'tooltip',
+      'show',
+      this.props.className
+    ), this.props.cssModule);
 
     return (
       <PopperContent
