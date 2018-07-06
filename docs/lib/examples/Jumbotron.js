1139d7f752a1dca4d87c6abdf8cb20a252daea79
Teoalmonte <teoalmonte@gmail.com>
2018-07-06 04:54:25 -0400

    docs(Jumbotron) fix typo (#1113)
    This change fixes a typo in the Jumbotrondocs where "typography" was misspelled.

--- docs/lib/examples/Jumbotron.js
+++ docs/lib/examples/Jumbotron.js
@@ -8,7 +8,7 @@ const Example = (props) => {
         <h1 className="display-3">Hello, world!</h1>
         <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
         <hr className="my-2" />
-        <p>It uses utility classes for typgraphy and spacing to space content out within the larger container.</p>
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
         <p className="lead">
           <Button color="primary">Learn More</Button>
         </p>
