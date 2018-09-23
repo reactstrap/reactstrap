b68316ebed637caaca55edc37eb6d69ba66ee3a0
Nisheet Sinvhal <nisheet1.sinvhal@gmail.com>
2018-09-24 00:41:29 +0530

    chore(UncontrolledButtonDropdown): Add docs
    Add docs as per issue #1219
    
    No breaking change

---
+++ docs/lib/examples/ButtonDropdownUncontrolled.js
@@ -0,0 +1,22 @@
+/* eslint react/no-multi-comp: 0, react/prop-types: 0 */
+import React from 'react';
+import { UncontrolledButtonDropdown, DropdownMenu, DropdownItem, DropdownToggle } from 'reactstrap';
+
+export default class Example extends React.Component {
+	render() {
+		return (
+			<UncontrolledButtonDropdown>
+				<DropdownToggle caret>
+					Dropdown
+      			</DropdownToggle>
+				<DropdownMenu>
+					<DropdownItem header>Header</DropdownItem>
+					<DropdownItem disabled>Action</DropdownItem>
+					<DropdownItem>Another Action</DropdownItem>
+					<DropdownItem divider />
+					<DropdownItem>Another Action</DropdownItem>
+				</DropdownMenu>
+			</UncontrolledButtonDropdown>
+		);
+	}
+}
