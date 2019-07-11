fe202246dface0b9f0ea5e16ce7e804442c9cd0b
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-07-11 03:02:09 -0700

    feat: Migrate Form doc examples to function components

--- docs/lib/examples/InputGridSizing.js
+++ docs/lib/examples/InputGridSizing.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { Col, Form, FormGroup, Label, Input } from 'reactstrap';
 
-export default class Example extends React.Component {
-  render() {
-    return (
-      <Form>
-        <FormGroup row>
-          <Label for="exampleEmail" sm={2} size="lg">Email</Label>
-          <Col sm={10}>
-            <Input type="email" name="email" id="exampleEmail" placeholder="lg" bsSize="lg" />
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="exampleEmail2" sm={2}>Email</Label>
-          <Col sm={10}>
-            <Input type="email" name="email" id="exampleEmail2" placeholder="default" />
-          </Col>
-        </FormGroup>
-      </Form>
-    );
-  }
+const Example = (props) => {
+  return (
+    <Form>
+      <FormGroup row>
+        <Label for="exampleEmail" sm={2} size="lg">Email</Label>
+        <Col sm={10}>
+          <Input type="email" name="email" id="exampleEmail" placeholder="lg" bsSize="lg" />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label for="exampleEmail2" sm={2}>Email</Label>
+        <Col sm={10}>
+          <Input type="email" name="email" id="exampleEmail2" placeholder="default" />
+        </Col>
+      </FormGroup>
+    </Form>
+  );
 }
+
+export default Example;
--- docs/lib/examples/LabelHidden.js
+++ docs/lib/examples/LabelHidden.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
-export default class Example extends React.Component {
-  render() {
-    return (
-      <Form inline>
-        <FormGroup>
-          <Label for="exampleEmail" hidden>Email</Label>
-          <Input type="email" name="email" id="exampleEmail" placeholder="Email" />
-        </FormGroup>
-        {' '}
-        <FormGroup>
-          <Label for="examplePassword" hidden>Password</Label>
-          <Input type="password" name="password" id="examplePassword" placeholder="Password" />
-        </FormGroup>
-        {' '}
-        <Button>Submit</Button>
-      </Form>
-    );
-  }
+const Example = (props) => {
+  return (
+    <Form inline>
+      <FormGroup>
+        <Label for="exampleEmail" hidden>Email</Label>
+        <Input type="email" name="email" id="exampleEmail" placeholder="Email" />
+      </FormGroup>
+      {' '}
+      <FormGroup>
+        <Label for="examplePassword" hidden>Password</Label>
+        <Input type="password" name="password" id="examplePassword" placeholder="Password" />
+      </FormGroup>
+      {' '}
+      <Button>Submit</Button>
+    </Form>
+  );
 }
+
+export default Example;
