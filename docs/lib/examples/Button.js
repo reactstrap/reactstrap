f158dfe1aeb44d52833867860d02bee99ec919a9
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-07-11 01:10:28 -0700

    feat: Migrate Button doc examples to function components

--- docs/lib/examples/Button.js
+++ docs/lib/examples/Button.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Button } from 'reactstrap';
 
-export default class Example extends React.Component {
-  render() {
-    return (
-      <div>
-        <Button color="primary">primary</Button>{' '}
-        <Button color="secondary">secondary</Button>{' '}
-        <Button color="success">success</Button>{' '}
-        <Button color="info">info</Button>{' '}
-        <Button color="warning">warning</Button>{' '}
-        <Button color="danger">danger</Button>{' '}
-        <Button color="link">link</Button>
-      </div>
-    );
-  }
+const Example = (props) => {
+  return (
+    <div>
+      <Button color="primary">primary</Button>{' '}
+      <Button color="secondary">secondary</Button>{' '}
+      <Button color="success">success</Button>{' '}
+      <Button color="info">info</Button>{' '}
+      <Button color="warning">warning</Button>{' '}
+      <Button color="danger">danger</Button>{' '}
+      <Button color="link">link</Button>
+    </div>
+  );
 }
+
+export default Example;
