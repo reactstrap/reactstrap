e4bc9c4cff2a27b68d85c26c7ab49d128c9598e5
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2019-07-11 18:40:07 -0700

    feat: Migrate Layout & ListGroup examples to function components

--- docs/lib/examples/ListGroupContextualClasses.js
+++ docs/lib/examples/ListGroupContextualClasses.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
-export default class Example extends React.Component {
-  render() {
-    return (
-      <ListGroup>
-        <ListGroupItem color="success">Cras justo odio</ListGroupItem>
-        <ListGroupItem color="info">Dapibus ac facilisis in</ListGroupItem>
-        <ListGroupItem color="warning">Morbi leo risus</ListGroupItem>
-        <ListGroupItem color="danger">Porta ac consectetur ac</ListGroupItem>
-      </ListGroup>
-    );
-  }
+const Example = (props) => {
+  return (
+    <ListGroup>
+      <ListGroupItem color="success">Cras justo odio</ListGroupItem>
+      <ListGroupItem color="info">Dapibus ac facilisis in</ListGroupItem>
+      <ListGroupItem color="warning">Morbi leo risus</ListGroupItem>
+      <ListGroupItem color="danger">Porta ac consectetur ac</ListGroupItem>
+    </ListGroup>
+  );
+
 }
+
+export default Example;
