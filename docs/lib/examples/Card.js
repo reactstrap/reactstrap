ed5ae0a62c8821c31de282eab6e9f84bd65bd58a
Deepak Sharma <dpkshrma@users.noreply.github.com>
2019-08-28 18:56:30 +0530

    Fix typo

--- docs/lib/examples/Card.js
+++ docs/lib/examples/Card.js
@@ -12,7 +12,7 @@ const Example = (props) => {
           <CardTitle>Card title</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
           <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <Button>Button</Button>03:26 PM - 
+          <Button>Button</Button>
         </CardBody>
       </Card>
     </div>
