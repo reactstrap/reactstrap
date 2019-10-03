5d6413b73ffa6ad5b2d9a7c3d196b0f46024a1b8
Gaurav Rathi <gaurav.rathi@live.com>
2019-10-03 17:56:51 +0530

    docs(Cards): fix broken link

--- docs/lib/examples/CardGroups.js
+++ docs/lib/examples/CardGroups.js
@@ -25,7 +25,7 @@ const Example = (props) => {
         </CardBody>
       </Card>
       <Card>
-        <CardImg top width="100%" src="/assets/static318x180.svg" alt="Card image cap" />
+        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
         <CardBody>
           <CardTitle>Card title</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
