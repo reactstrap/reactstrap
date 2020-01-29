0cde3d045d86a449940072defc1a1b46b81bd724
kyletsang <6854874+kyletsang@users.noreply.github.com>
2020-01-28 17:49:05 -0800

    test: Fix failing unit test and warnings

--- src/__tests__/InputGroupButtonDropdown.spec.js
+++ src/__tests__/InputGroupButtonDropdown.spec.js
@@ -4,7 +4,7 @@ import { InputGroupButtonDropdown, Dropdown } from '../';
 
 describe('InputGroupButtonDropdown', () => {
   it('should render Dropdown', () => {
-    const wrapper = shallow(<InputGroupButtonDropdown>Yo!</InputGroupButtonDropdown>);
+    const wrapper = shallow(<InputGroupButtonDropdown addonType="prepend">Yo!</InputGroupButtonDropdown>);
 
     expect(wrapper.type()).toBe(Dropdown);
   });
