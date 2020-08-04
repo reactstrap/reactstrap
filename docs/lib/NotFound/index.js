23481711d8874d1e75b2840f15841a2b98f71bf3
Thach Nguyen <nthachus@gmail.com>
2020-08-04 23:09:01 +0700

    - Correct display of `NotFound` page across the site - Correct height of the `.wrapper` by padding for `Banner`

--- docs/lib/NotFound/index.js
+++ docs/lib/NotFound/index.js
@@ -1,31 +1,26 @@
 import React from 'react';
-import { Button, Container, Row, Col } from 'reactstrap';
+import { Button, Container, Jumbotron } from 'reactstrap';
 import { Link } from 'react-router';
 import Helmet from 'react-helmet';
 
-export default () => {
-  return (
-    <div>
-      <Helmet title="404 Page Not Found" />
-      <section className="jumbotron text-center mb-3">
-        <Container>
-          <Row>
-            <Col>
-              <p className="lead">
-                <img src="/assets/logo.png" alt="" width="150px" />
-              </p>
-              <h1 className="jumbotron-heading display-4">404 - Not Found</h1>
-              <p className="lead">
-                Can't find what you're looking for? <a href="https://github.com/reactstrap/reactstrap/issues/new">Open</a> up an issue.
-              </p>
-              <p>
-                <Button outline color="danger" className="mr-1" tag={Link} to="/">Get Started</Button>
-                <Button color="danger" tag={Link} to="/components/">View Components</Button>
-              </p>
-            </Col>
-          </Row>
-        </Container>
-      </section>
-    </div>
-  );
-};
+export default () => (
+  <main>
+    <Helmet title="404 Page Not Found" />
+    <Jumbotron className="text-center mb-3">
+      <Container>
+        <p className="lead">
+          <img src="/assets/logo.png" alt="logo" width="150px" />
+        </p>
+        <h1 className="jumbotron-heading display-4">404 - Not Found</h1>
+        <p className="lead">
+          Can't find what you're looking for? <a href="https://github.com/reactstrap/reactstrap/issues/new">Open</a> up
+          an issue.
+        </p>
+        <p>
+          <Button outline color="danger" className="mr-3" tag={Link} to="/">Get Started</Button>
+          <Button color="danger" tag={Link} to="/components/">View Components</Button>
+        </p>
+      </Container>
+    </Jumbotron>
+  </main>
+);
--- docs/lib/UI/Nav.js
+++ docs/lib/UI/Nav.js
@@ -3,48 +3,41 @@ import { Link } from 'react-router';
 import { NavbarToggler, Container, Collapse, Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import DocSearch from './DocSearch';
 
-export default class UINav extends React.Component {
-  constructor(props) {
-    super(props);
+export default () => {
+  const [showNavbar, setShowNavbar] = React.useState(false);
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      showNavbar: false
-    };
-  }
-  toggleNavbar(e) {
+  const toggleNavbar = e => {
     e.preventDefault();
-    this.setState({
-      showNavbar: !this.state.showNavbar
-    });
-  }
-  render() {
-    return (
-        <Navbar className="header" color="faded" light expand="md" style={{ marginTop: "100px" }}>
-          <Container>
-            <NavbarBrand className="mr-auto" tag={Link} to="/">reactstrap</NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} />
-            <Collapse navbar isOpen={this.state.showNavbar}>
-              <Nav navbar className="ml-sm-auto">
-                <NavItem>
-                  <DocSearch />
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} to="/components/" activeClassName="active">Components</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} to="/utilities/" activeClassName="active">Utilities</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink className="font-weight-bold" tag={Link} to="/premium-themes/" activeClassName="active">Premium Themes</NavLink>
-                </NavItem>
-              </Nav>
-            </Collapse>
-          </Container>
-        </Navbar>
-    );
-  }
-}
+    setShowNavbar(!showNavbar);
+  };
+
+  return (
+    <Navbar className="header" color="faded" light expand="md">
+      <Container>
+        <NavbarBrand className="mr-auto" tag={Link} to="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} />
+        <Collapse navbar isOpen={showNavbar}>
+          <Nav navbar className="ml-sm-auto">
+            <NavItem>
+              <DocSearch />
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} to="/components/" activeClassName="active">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} to="/utilities/" activeClassName="active">Utilities</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink className="font-weight-bold" tag={Link} to="/premium-themes/" activeClassName="active">
+                Premium Themes
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Container>
+    </Navbar>
+  );
+};
