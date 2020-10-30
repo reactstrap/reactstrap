cd669d47f2971c4c5a7c0e4b934a63c32a474263
Kyle Tsang <6854874+kyletsang@users.noreply.github.com>
2020-10-30 12:11:20 -0700

    docs(FormFeedback): Document position relative requirement and fix tooltip example (#2003)

--- docs/lib/examples/FormFeedback.js
+++ docs/lib/examples/FormFeedback.js
@@ -28,13 +28,13 @@ const Example = (props) => {
         <FormFeedback tooltip>You will not be able to see this</FormFeedback>
         <FormText>Example help text that remains unchanged.</FormText>
       </FormGroup>
-      <FormGroup>
+      <FormGroup className="position-relative">
         <Label for="exampleEmail">Valid input</Label>
         <Input valid />
         <FormFeedback valid tooltip>Sweet! that name is available</FormFeedback>
         <FormText>Example help text that remains unchanged.</FormText>
       </FormGroup>
-      <FormGroup>
+      <FormGroup className="position-relative">
         <Label for="examplePassword">Invalid input</Label>
         <Input invalid />
         <FormFeedback tooltip>Oh noes! that name is already taken</FormFeedback>
