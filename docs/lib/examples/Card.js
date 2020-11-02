42e93c181d1352640d4677ae2812b1eb2182f104
koenvg <koen.van.geert@gmail.com>
2020-11-02 07:03:43 +0100

    docs(Card): Update Card examples (#1998)
    Update card title and subtitle in the examples to be in sync with the upstream bootstrap docs
    
    Closes #1991
    
    Co-authored-by: Kyle Tsang <6854874+kyletsang@users.noreply.github.com>

--- docs/lib/examples/Card.js
+++ docs/lib/examples/Card.js
@@ -10,8 +10,8 @@ const Example = (props) => {
       <Card>
         <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
         <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardTitle tag="h5">Card title</CardTitle>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
           <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
           <Button>Button</Button>
         </CardBody>
--- docs/lib/examples/CardOutline.js
+++ docs/lib/examples/CardOutline.js
@@ -5,32 +5,32 @@ const Example = (props) => {
   return (
     <div>
       <Card body outline color="secondary">
-        <CardTitle>Special Title Treatment</CardTitle>
+        <CardTitle tag="h5">Special Title Treatment</CardTitle>
         <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
         <Button>Button</Button>
       </Card>
       <Card body outline color="primary">
-        <CardTitle>Special Title Treatment</CardTitle>
+        <CardTitle tag="h5">Special Title Treatment</CardTitle>
         <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
         <Button color="secondary">Button</Button>
       </Card>
       <Card body outline color="success">
-        <CardTitle>Special Title Treatment</CardTitle>
+        <CardTitle tag="h5">Special Title Treatment</CardTitle>
         <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
         <Button color="secondary">Button</Button>
       </Card>
       <Card body outline color="info">
-        <CardTitle>Special Title Treatment</CardTitle>
+        <CardTitle tag="h5">Special Title Treatment</CardTitle>
         <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
         <Button color="secondary">Button</Button>
       </Card>
       <Card body outline color="warning">
-        <CardTitle>Special Title Treatment</CardTitle>
+        <CardTitle tag="h5">Special Title Treatment</CardTitle>
         <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
         <Button color="secondary">Button</Button>
       </Card>
       <Card body outline color="danger">
-        <CardTitle>Special Title Treatment</CardTitle>
+        <CardTitle tag="h5">Special Title Treatment</CardTitle>
         <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
         <Button color="secondary">Button</Button>
       </Card>
