a330ce65e1a38dd0a14e1bc0ca37f86740964df1
Harry Yu <hy.harry.yu@gmail.com>
2020-12-22 20:27:26 -0800

    feat: add positionFixed prop to Tooltip and Popover (#2039)
    * feat: add positionFixed prop to Tooltip and Popover
    
    * fix: added positionFixed to PopperContent PropTypes
    
    Co-authored-by: Kyle Tsang <6854874+kyletsang@users.noreply.github.com>

--- src/TooltipPopoverWrapper.js
+++ src/TooltipPopoverWrapper.js
@@ -32,6 +32,7 @@ export const propTypes = {
     PropTypes.number
   ]),
   modifiers: PropTypes.object,
+  positionFixed: PropTypes.bool,
   offset: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   innerRef: PropTypes.oneOfType([
     PropTypes.func,
@@ -345,6 +346,7 @@ class TooltipPopoverWrapper extends React.Component {
       popperClassName,
       container,
       modifiers,
+      positionFixed,
       offset,
       fade,
       flip,
@@ -370,6 +372,7 @@ class TooltipPopoverWrapper extends React.Component {
         popperClassName={popperClasses}
         container={container}
         modifiers={modifiers}
+        positionFixed={positionFixed}
         offset={offset}
         cssModule={cssModule}
         fade={fade}
