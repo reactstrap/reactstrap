f98d287694bae35607ab888602af33beed0c9433
Phoebe Gao <phoebe.gao@appfolio.com>
2021-01-28 14:36:23 -0800

    add accordion tests

--- src/__tests__/AccordionHeader.spec.js
+++ src/__tests__/AccordionHeader.spec.js
@@ -1,25 +1,48 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { AccordionHeader } from '../';
+import { mount } from 'enzyme';
+import { AccordionHeader, AccordionContext } from '../';
 
 describe('AccordionHeader', () => {
   it('should render with "accordion-header" class', () => {
-    const wrapper = shallow(<AccordionHeader />);
+    const wrapper = mount(<AccordionHeader targetId="cool-accordion" />);
 
-    expect(wrapper.hasClass('accordion-header')).toBe(true);
+    expect(wrapper.find('.accordion-header').find('.accordion-button.collapsed').length).toBe(1);
   });
 
   it('should render additional classes', () => {
-    const wrapper = shallow(<AccordionHeader className="other" />);
+    const wrapper = mount(<AccordionHeader targetId="cool-accordion" className="other" />);
 
-    expect(wrapper.hasClass('other')).toBe(true);
-    expect(wrapper.hasClass('accordion-header')).toBe(true);
+    expect(wrapper.find('.accordion-header').hasClass('other')).toBe(true);
   });
 
   it('should render custom tag', () => {
-    const wrapper = shallow(<AccordionHeader tag="div" />);
+    const wrapper = mount(<AccordionHeader targetId="cool-accordion" tag="div" />);
 
-    expect(wrapper.hasClass('accordion-header')).toBe(true);
-    expect(wrapper.find('div').length).toBe(1);
+    expect(wrapper.find('div.accordion-header').length).toBe(1);
+  });
+
+  it('should be open if openId == targetId', () => {
+    const wrapper = mount(
+      <AccordionContext.Provider value={{ openId: 'cool-accordion' }}>
+        <AccordionHeader targetId="cool-accordion" />
+      </AccordionContext.Provider>
+    );
+
+    expect(wrapper.find('.accordion-header').find('.accordion-button.collapsed').length).toBe(0);
+  });
+
+  it('should toggle collapse if accordion-button is clicked', () => {
+    const toggle = jest.fn();
+
+    const wrapper = mount(
+      <AccordionContext.Provider value={{ toggle }}>
+        <AccordionHeader targetId="cool-accordion" />
+      </AccordionContext.Provider>
+    );
+
+    const accordionButton = wrapper.find('.accordion-header').find('.accordion-button');
+    accordionButton.simulate('click');
+
+    expect(toggle.mock.calls.length).toBe(1);
   });
 });
