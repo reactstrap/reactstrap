39faf807a759fc3874d5cec08e87706ece643c0a
Phoebe Gao <phoebe.gao@appfolio.com>
2021-01-28 13:52:52 -0800

    update accordion type definitions

--- types/lib/AccordionHeader.d.ts
+++ types/lib/AccordionHeader.d.ts
@@ -5,6 +5,7 @@ export interface AccordionHeaderProps extends React.HTMLAttributes<HTMLElement> 
   tag?: React.ElementType;
   cssModule?: CSSModule;
   innerRef?: React.Ref<HTMLElement>;
+  targetId: string;
 }
 
 declare class AccordionHeader extends React.Component<AccordionHeaderProps> {}
