f9666332c781736cd3ebf18cc103b7a7b7df770e
Phoebe Gao <phoebe.gao@appfolio.com>
2021-01-29 14:51:57 -0800

    chore(dep): upgrade to popper 2

--- docs/lib/examples/PopoverUpdate.js
+++ docs/lib/examples/PopoverUpdate.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import { Button, UncontrolledPopover, PopoverHeader, PopoverBody, Collapse } from 'reactstrap';
 
-const PopoverContent = ({ scheduleUpdate }) => {
+const PopoverContent = ({ update }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <PopoverHeader>Schedule Update</PopoverHeader>
+      <PopoverHeader>Update</PopoverHeader>
       <PopoverBody>
         <Button onClick={() => setIsOpen(!isOpen)}>Click me</Button>
-        <Collapse isOpen={isOpen} onEntered={scheduleUpdate} onExited={scheduleUpdate}>
+        <Collapse isOpen={isOpen} onEntered={update} onExited={update}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
           labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
           laboris nisi ut aliquip ex ea commodo consequat.
@@ -22,12 +22,12 @@ const PopoverContent = ({ scheduleUpdate }) => {
 const Example = (props) => {
   return (
     <div className="text-center">
-      <Button id="ScheduleUpdateButton" type="button">
+      <Button id="updateButton" type="button">
         Open Popover
       </Button>
-      <UncontrolledPopover trigger="click" placement="top" target="ScheduleUpdateButton">
-        {({ scheduleUpdate }) => (
-          <PopoverContent scheduleUpdate={scheduleUpdate} />
+      <UncontrolledPopover trigger="click" placement="top" target="updateButton">
+        {({ update }) => (
+          <PopoverContent update={update} />
         )}
       </UncontrolledPopover>
     </div>
