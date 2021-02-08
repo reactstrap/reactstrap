9742da600141895be23246ac3b448e9aa667bd37
Phoebe Gao <phoebe.gao@appfolio.com>
2021-02-08 13:45:08 -0800

    fix(InputGroup): toggle if clicking dropdown input

--- src/__tests__/InputGroup.spec.js
+++ src/__tests__/InputGroup.spec.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { InputGroup } from '../';
+import React, { useState } from 'react';
+import { shallow, mount } from 'enzyme';
+import { InputGroup, DropdownMenu, DropdownToggle, DropdownItem, Input } from '../';
 import Dropdown from '../Dropdown';
 
 describe('InputGroup', () => {
@@ -41,9 +41,34 @@ describe('InputGroup', () => {
     expect(wrapper.type()).toBe('main');
   });
 
-  it('should render Dropdown when type is dropdown', () => {
-    const wrapper = shallow(<InputGroup type="dropdown" />);
+  describe('When type="dropdown"', () => {
+    it('should render Dropdown', () => {
+      const wrapper = shallow(<InputGroup type="dropdown" />);
+  
+      expect(wrapper.type()).toBe(Dropdown);
+    });
 
-    expect(wrapper.type()).toBe(Dropdown);
+    it('should call toggle when input is clicked', () => {
+      jest.spyOn(Dropdown.prototype, 'toggle');
+
+      const wrapper = mount(
+        <InputGroup type="dropdown" isOpen={true} toggle={() => {}}>
+          <Input />
+          <DropdownToggle>Toggle</DropdownToggle>
+          <DropdownMenu right>
+            <DropdownItem>Test</DropdownItem>
+            <DropdownItem id="divider" divider />
+          </DropdownMenu>
+        </InputGroup>
+      , { attachTo: document.body });
+
+      expect(Dropdown.prototype.toggle.mock.calls.length).toBe(0);
+
+      document.querySelector('input.form-control').click();
+
+      expect(Dropdown.prototype.toggle.mock.calls.length).toBe(1);
+
+      wrapper.detach();
+    });
   });
 });
