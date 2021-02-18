65bf0f0dae5732dc4a7daef59717fa35c0a2a07d
Phoebe Gao <phoebe.gao@appfolio.com>
2021-02-18 15:43:00 -0800

    feat!: update positionedFix prop to strategy
    Breaking change to update the old `positionFixed` popper prop to the new `strategy` prop.

--- types/lib/Popover.d.ts
+++ types/lib/Popover.d.ts
@@ -25,7 +25,7 @@ export interface PopoverProps extends React.HTMLAttributes<HTMLElement> {
   placementPrefix?: string;
   delay?: number | { show: number; hide: number };
   modifiers?: Popper.Modifiers;
-  positionFixed?: boolean;
+  strategy?: string;
   cssModule?: CSSModule;
   fade?: boolean;
   flip?: boolean;
