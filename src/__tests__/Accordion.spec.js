6ab7e9f7c8d8ce3d7bb0f4b65030b371cd111704
bestguy <7zark7@gmail.com>
2021-05-21 08:31:34 -0700

    Correct typo in Accordion story

--- src/__tests__/Accordion.spec.js
+++ src/__tests__/Accordion.spec.js
@@ -4,19 +4,19 @@ import { Accordion } from '../';
 
 describe('Accordion', () => {
   it('should render with "accordion" class', () => {
-    const wrapper = mount(<Accordion openId="this accordion" togggle={() => {}} />);
+    const wrapper = mount(<Accordion openId="this accordion" toggle={() => {}} />);
 
     expect(wrapper.find('.accordion').length).toBe(1);
   });
 
   it('should render additional classes', () => {
-    const wrapper = mount(<Accordion className="other" openId="this accordion" togggle={() => {}} />);
+    const wrapper = mount(<Accordion className="other" openId="this accordion" toggle={() => {}} />);
 
     expect(wrapper.find('.accordion').hasClass('other')).toBe(true);
   });
 
   it('should render custom tag', () => {
-    const wrapper = mount(<Accordion tag="main" openId="this accordion" togggle={() => {}} />);
+    const wrapper = mount(<Accordion tag="main" openId="this accordion" toggle={() => {}} />);
 
     expect(wrapper.find('main.accordion').length).toBe(1);
   });
