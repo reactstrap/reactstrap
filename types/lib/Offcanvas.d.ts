d08c8aff290166c00fe2c5b04dac1112df37c7dc
bestguy <7zark7@gmail.com>
2021-08-20 13:39:22 -0700

    Update backdrop and overflow
    Correct backdrop click and overflow
    Support scrollable

--- types/lib/Offcanvas.d.ts
+++ types/lib/Offcanvas.d.ts
@@ -6,32 +6,32 @@ export type Placement = 'start' | 'end' | 'left' | 'right' | 'bottom' | 'top';
 
 export interface OffcanvasProps extends React.HTMLAttributes<HTMLElement> {
   [key: string]: any;
-  isOpen?: boolean;
   autoFocus?: boolean;
-  toggle?: React.KeyboardEventHandler<any> | React.MouseEventHandler<any>;
-  keyboard?: boolean;
   backdrop?: boolean | 'static';
-  placement?: Placement;
-  scrollable?: boolean;
+  backdropClassName?: string;
+  backdropTransition?: FadeProps;
+  container?: string | HTMLElement | React.RefObject<HTMLElement>;
+  contentClassName?: string;
+  cssModule?: CSSModule;
+  fade?: boolean;
+  innerRef?: React.Ref<HTMLElement>;
+  isOpen?: boolean;
+  keyboard?: boolean;
+  labelledBy?: string;
+  offcanvasClassName?: string;
+  offcanvasTransition?: FadeProps;
+  onClosed?: () => void;
   onEnter?: () => void;
   onExit?: () => void;
   onOpened?: () => void;
-  onClosed?: () => void;
-  cssModule?: CSSModule;
-  wrapClassName?: string;
-  offcanvasClassName?: string;
-  backdropClassName?: string;
-  contentClassName?: string;
-  zIndex?: number | string;
-  fade?: boolean;
-  backdropTransition?: FadeProps;
-  offcanvasTransition?: FadeProps;
-  labelledBy?: string;
-  unmountOnClose?: boolean;
+  placement?: Placement;
   returnFocusAfterClose?: boolean;
-  container?: string | HTMLElement | React.RefObject<HTMLElement>;
-  innerRef?: React.Ref<HTMLElement>;
+  scrollable?: boolean;
+  toggle?: React.KeyboardEventHandler<any> | React.MouseEventHandler<any>;
   trapFocus?: boolean;
+  unmountOnClose?: boolean;
+  wrapClassName?: string;
+  zIndex?: number | string;
 }
 
 declare class Offcanvas extends React.Component<OffcanvasProps> {}
