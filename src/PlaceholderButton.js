6eb201b57f59d1ab5fc3ed3891b0996f2e4ba1d2
illiteratewriter <manukoshyabraham@gmail.com>
2021-08-21 14:35:49 +0530

    feat(Placeholder): add component

---
+++ src/PlaceholderButton.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { mapToCssModules, tagPropType } from './utils';
+import Button from "./Button";
+import { getColumnClasses } from './Col';
+
+const propTypes = {
+  size: PropTypes.string,
+  color: PropTypes.string,
+  outline: PropTypes.bool,
+  className: PropTypes.string,
+  tag: tagPropType
+}
+
+const defaultProps = {
+  color: 'secondary',
+  tag: Button
+}
+
+const PlaceholderButton = (props) => {
+  let {
+    size,
+    color,
+    outline,
+    cssModule,
+    className,
+    tag: Tag,
+    ...attributes
+  } = props;
+
+  let { attributes: modifiedAttributes, colClasses } = getColumnClasses(attributes, cssModule)
+
+  const classes = mapToCssModules(classNames(
+    "placeholder",
+    className,
+    colClasses
+  ), cssModule);
+
+  return (
+    <Button {...modifiedAttributes} color="primary" className={classes} disabled={true}></Button>
+  )
+}
+
+PlaceholderButton.propTypes = propTypes;
+PlaceholderButton.defaultProps = defaultProps;
+
+export default PlaceholderButton 
