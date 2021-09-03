8ee4dce55fd925070016153edb28959581ce5545
bestguy <7zark7@gmail.com>
2021-09-02 21:56:08 -0700

    Update home page

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,9 +1,21 @@
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
+    hideNoControlsWarning: true,
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
+  docs: {
+    source: {
+      state: 'open'
+    }
+  },
+  options: {
+    storySort: {
+      order: ['Home', '*']
+    }
+  },
+  viewMode: 'docs'
 }
