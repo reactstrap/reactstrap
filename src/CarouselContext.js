0d809f083e9f8c0f7d70b943ad7175a4d707b27d
bestguy <7zark7@gmail.com>
2021-09-05 11:38:34 -0700

    refactor(Carousel): Update Carousel from legacy context

---
+++ src/CarouselContext.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+/**
+ * CarouselContext
+ * {
+ *  direction: PropTypes.oneOf(['start', 'end']).isRequired,
+ * }
+ */
+export const CarouselContext = React.createContext({});
