f2c78ddafd59e9ac9736b782d9c764f97703d241
bestguy <7zark7@gmail.com>
2021-09-09 21:28:06 -0700

    Correct Breadcrumb props

--- stories/examples/BreadcrumbProps.js
+++ stories/examples/BreadcrumbProps.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Breadcrumb } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import Props from './Props';
 
 const Example = () => (
-  <Props component={Breadcrumb} />
+  <Props components={[Breadcrumb, BreadcrumbItem]} />
 );
 
 export default Example;
