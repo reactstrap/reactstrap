077a56a97d78221b47790d479065931c858cc571
Phoebe Gao <phoebe.gao@appfolio.com>
2021-09-14 10:49:27 -0700

    chore(lint): fix spacing in src/utils.js

--- src/utils.js
+++ src/utils.js
@@ -149,10 +149,10 @@ export const tagPropType = PropTypes.oneOfType([
 // These are all setup to match what is in the bootstrap _variables.scss
 // https://github.com/twbs/bootstrap/blob/v4-dev/scss/_variables.scss
 export const TransitionTimeouts = {
-  Fade:     150, // $transition-fade
-  Collapse: 350, // $transition-collapse
-  Modal:    300, // $modal-transition
-  Carousel: 600, // $carousel-transition
+  Fade:      150, // $transition-fade
+  Collapse:  350, // $transition-collapse
+  Modal:     300, // $modal-transition
+  Carousel:  600, // $carousel-transition
   Offcanvas: 300, // $offcanvas-transition
 };
 
