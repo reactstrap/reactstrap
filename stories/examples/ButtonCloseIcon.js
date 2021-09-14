6ec03ed1516453339f70039336b6e6d38098ea50
bestguy <7zark7@gmail.com>
2021-09-13 20:33:20 -0700

    Correct close story

--- stories/examples/ButtonCloseIcon.js
+++ stories/examples/ButtonCloseIcon.js
@@ -3,35 +3,7 @@ import { Button, Card, CardBody, CardText, CardGroup, CardTitle } from 'reactstr
 
 const Example = (props) => (
   <div>
-    <CardGroup>
-      <Card>
-        <CardBody>
-          <CardTitle>
-
-            <Button close />
-
-          </CardTitle>
-          <CardText>Default close icon</CardText>
-        </CardBody>
-      </Card>
-
-      <Card>
-        <CardBody>
-          <CardTitle>
-
-            <Button close aria-label="Cancel">
-              <span aria-hidden>&ndash;</span>
-            </Button>
-
-          </CardTitle>
-          <CardText>
-          Custom content and aria-label
-          </CardText>
-        </CardBody>
-      </Card>
-    </CardGroup>
-
-
+    <Button close />
   </div>
 );
 
