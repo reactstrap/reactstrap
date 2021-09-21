e3221b836db9ac9e4b75f2d56949ad49b31b4d3a
Phoebe Gao <phoebe.gao@appfolio.com>
2021-09-20 21:48:40 -0700

    chore(test): update react-app-rewired config-overrides to work with ESModules
    See https://github.com/timarney/react-app-rewired/issues/431

--- config-overrides.js
+++
@@ -1,3 +0,0 @@
-const { override, addBabelPlugins } = require('customize-cra');
-
-module.exports = override(...addBabelPlugins('@babel/plugin-proposal-export-default-from', '@babel/plugin-proposal-export-namespace-from'));
