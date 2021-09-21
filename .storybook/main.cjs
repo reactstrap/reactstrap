d7e501a7c11fe6444968217ade83c914e1f346d8
Phoebe Gao <phoebe.gao@appfolio.com>
2021-09-20 17:20:51 -0700

    chore(docs): Add themes to storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   stories: [
-    '../stories/*.stories.mdx',
-    '../stories/*.stories.js'
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.js'
   ],
   addons: [
     '@storybook/addon-links',
