6cd2bc36e3b462ff701982a119332a43d0cb8fea
illiteratewriter <manukoshyabraham@gmail.com>
2022-06-01 21:12:28 +0530

    docs(Placeholder): add placeholder docs

---
+++ stories/Placeholder.stories.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default {
+  title: 'Components/Placeholder',
+  parameters: {
+    docs: {
+      description: {
+        component: `
+  [Bootstrap Placeholder](https://getbootstrap.com/docs/5.2/components/placeholders/)
+
+  Use loading placeholders for your components or pages to indicate something may still be loading.   
+        `
+      }
+    }
+  }
+}
+
+export { default as Placeholder } from './examples/Placeholder';
+export { default as Color } from './examples/PlaceholderColor';
+export { default as Sizing } from './examples/PlaceholderSize';
+export { default as Animation } from './examples/PlaceholderAnimation';
+export { default as Props } from './examples/PlaceholderProps';
---
+++ stories/examples/PlaceholderColor.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import { Placeholder } from 'reactstrap';
+
+const Example = (args) => {
+  return (
+    <>
+      <Placeholder xs={12} /> 
+
+      <Placeholder xs={12} color='primary'/>
+      <Placeholder xs={12} color='secondary'/>
+      <Placeholder xs={12} color='success'/>
+      <Placeholder xs={12} color='danger'/>
+      <Placeholder xs={12} color='warning'/>
+      <Placeholder xs={12} color='info'/>
+      <Placeholder xs={12} color='light'/>
+      <Placeholder xs={12} color='dark'/>
+    </>
+  )
+}
+
+export default Example;
