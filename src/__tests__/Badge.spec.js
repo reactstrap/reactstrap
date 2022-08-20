20469a83b4636ecc43e99cd6937bdc56799d8946
illiteratewriter <manukoshyabraham@gmail.com>
2022-08-20 12:56:10 +0530

    chore: use util functions in Badge tests

--- src/__tests__/Badge.spec.js
+++ src/__tests__/Badge.spec.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react';
 import { Badge } from '..';
-import { testForCustomTag, testForDefaultTag } from '../testUtils';
+import {
+  testForChildrenInComponent,
+  testForCustomTag,
+  testForDefaultClass,
+  testForDefaultTag,
+} from '../testUtils';
 
 describe('Badge', () => {
   it('should render a span by default', () => {
@@ -9,31 +14,41 @@ describe('Badge', () => {
   });
 
   it('should render an anchor when when href is provided', () => {
-    render(<Badge href="#" data-testid="badge">Yo!</Badge>);
-    expect(screen.getByTestId('badge').tagName.toLowerCase()).toBe('a')
+    render(
+      <Badge href="#" data-testid="badge">
+        Yo!
+      </Badge>,
+    );
+    expect(screen.getByTestId('badge').tagName.toLowerCase()).toBe('a');
   });
 
   it('should render a custom tag when provided', () => {
-    testForCustomTag(Badge)
+    testForCustomTag(Badge);
   });
 
   it('should render children', () => {
-    render(<Badge>Yo!</Badge>);
-    expect(screen.getByText('Yo!')).toBeInTheDocument();
+    testForChildrenInComponent(Badge);
   });
 
   it('should render badges with secondary color', () => {
-    render(<Badge data-testid="badge">Default Badge</Badge>);
-    expect(screen.getByTestId('badge')).toHaveClass('bg-secondary');
+    testForDefaultClass(Badge, 'bg-secondary');
   });
 
   it('should render Badges with other colors', () => {
-    render(<Badge color="danger" data-testid="badge">Danger Badge</Badge>);
+    render(
+      <Badge color="danger" data-testid="badge">
+        Danger Badge
+      </Badge>,
+    );
     expect(screen.getByTestId('badge')).toHaveClass('bg-danger');
   });
 
   it('should render Badges as pills', () => {
-    render(<Badge pill data-testid="badge">Pill Badge</Badge>);
+    render(
+      <Badge pill data-testid="badge">
+        Pill Badge
+      </Badge>,
+    );
     expect(screen.getByTestId('badge')).toHaveClass('rounded-pill');
   });
 });
