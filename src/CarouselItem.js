cec7ab3a88b5ea079d384291ee8bb27eae60eda2
illiteratewriter <manukoshyabraham@gmail.com>
2022-08-21 18:48:14 +0530

    fix(#2421): carousel sliding in wrong direction

--- src/CarouselItem.js
+++ src/CarouselItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { Transition } from 'react-transition-group';
+import { CarouselContext } from './CarouselContext';
 import {
   mapToCssModules,
   TransitionTimeouts,
@@ -128,8 +129,6 @@ CarouselItem.defaultProps = {
   slide: true,
 };
 
-CarouselItem.contextTypes = {
-  direction: PropTypes.string,
-};
+CarouselItem.contextType = CarouselContext;
 
 export default CarouselItem;
