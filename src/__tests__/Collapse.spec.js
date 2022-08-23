d33e7ab89f0b83a95abe20044876eb465dc6b60b
illiteratewriter <manukoshyabraham@gmail.com>
2022-08-23 16:55:12 +0530

    test(Collapse): migrate tests to RTL

--- src/__tests__/Collapse.spec.js
+++ src/__tests__/Collapse.spec.js
@@ -1,86 +1,76 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import { Collapse } from '..';
 
 describe('Collapse', () => {
-  let isOpen;
-  let wrapper;
-  const toggle = () => {
-    isOpen = !isOpen;
-    if (wrapper != null) {
-      wrapper.setProps({ isOpen });
-    }
-  };
-
   beforeEach(() => {
-    isOpen = false;
     jest.useFakeTimers();
   });
 
   afterEach(() => {
-    // fast forward time for collapse to fade out
-    jest.runTimersToTime(400);
     jest.clearAllTimers();
   });
 
   it('should render children', () => {
-    wrapper = mount(
-      <Collapse>
-        <p>hello</p>
-      </Collapse>,
-    );
-    expect(wrapper.find('p').text()).toBe('hello');
+    render(<Collapse>Hello</Collapse>);
+    expect(screen.getByText(/hello/i)).toBeInTheDocument();
   });
 
   it('works with strict mode', () => {
     const spy = jest.spyOn(console, 'error');
-    wrapper = mount(
+    render(
       <React.StrictMode>
         <Collapse />
       </React.StrictMode>,
     );
-    expect(wrapper.instance()).toBeTruthy();
     expect(spy).not.toHaveBeenCalled();
   });
 
-  it('should have default isOpen value', () => {
-    wrapper = shallow(<Collapse />);
-    expect(wrapper.instance().props.isOpen).toEqual(false);
+  it('should have default isOpen value as false', () => {
+    render(<Collapse>Hello</Collapse>);
+    expect(screen.getByText(/hello/i)).not.toHaveClass('show');
   });
 
   it('should render with class "collapse"', () => {
-    wrapper = mount(<Collapse />);
-    expect(wrapper.find('div').hasClass('collapse')).toEqual(true);
+    render(<Collapse>Hello</Collapse>);
+    expect(screen.getByText(/hello/i)).toHaveClass('collapse');
   });
 
   it('should render with class "collapse-horizontal" if it has prop horizontal', () => {
-    wrapper = mount(<Collapse horizontal />);
-    expect(wrapper.find('div').hasClass('collapse-horizontal')).toEqual(true);
+    render(<Collapse horizontal>Hello</Collapse>);
+    expect(screen.getByText(/hello/i)).toHaveClass('collapse-horizontal');
   });
 
   it('should render with class "navbar-collapse" if it has prop navbar', () => {
-    wrapper = mount(<Collapse navbar />);
-    expect(wrapper.find('div').hasClass('navbar-collapse')).toEqual(true);
+    render(<Collapse navbar>Hello</Collapse>);
+    expect(screen.getByText(/hello/i)).toHaveClass('navbar-collapse');
   });
 
   it('should render with class "show" when isOpen is true', () => {
-    wrapper = mount(<Collapse isOpen />);
-    expect(wrapper.find('div').hasClass('show')).toEqual(true);
+    render(<Collapse isOpen>Hello</Collapse>);
+    expect(screen.getByText(/hello/i)).toHaveClass('show');
   });
 
   it('should set height to null when isOpen is true', () => {
-    wrapper = shallow(<Collapse isOpen />);
-    expect(wrapper.state('dimension')).toBe(null);
+    render(<Collapse isOpen data-testid="collapse" />);
+    expect(screen.getByTestId('collapse').style.height).toBe('');
   });
 
   it('should not set height when isOpen is false', () => {
-    wrapper = shallow(<Collapse isOpen={false} />);
-    expect(wrapper.state('dimension')).toBe(null);
+    render(<Collapse isOpen={false} data-testid="collapse" />);
+    expect(screen.getByTestId('collapse').style.height).toBe('');
   });
 
   it('should forward all styles', () => {
-    wrapper = mount(<Collapse isOpen style={{ backgroundColor: 'black' }} />);
-    expect(wrapper.find('div').prop('style').backgroundColor).toBe('black');
+    render(
+      <Collapse
+        isOpen={false}
+        data-testid="collapse"
+        style={{ backgroundColor: 'black' }}
+      />,
+    );
+    expect(screen.getByTestId('collapse').style.backgroundColor).toBe('black');
   });
 
   it('should forward all callbacks', () => {
@@ -92,56 +82,43 @@ describe('Collapse', () => {
       onExiting: jest.fn(),
       onExited: jest.fn(),
     };
-    wrapper = mount(<Collapse isOpen={isOpen} {...callbacks} />);
-    toggle();
+    const { rerender } = render(<Collapse isOpen={false} {...callbacks} />);
+    rerender(<Collapse isOpen {...callbacks} />);
     expect(callbacks.onEnter).toHaveBeenCalled();
     expect(callbacks.onEntering).toHaveBeenCalled();
     expect(callbacks.onEntered).not.toHaveBeenCalled();
     jest.runTimersToTime(350);
     expect(callbacks.onEntered).toHaveBeenCalled();
     expect(callbacks.onExit).not.toHaveBeenCalled();
-
-    toggle();
+    rerender(<Collapse isOpen={false} {...callbacks} />);
     expect(callbacks.onExit).toHaveBeenCalled();
     expect(callbacks.onExiting).toHaveBeenCalled();
     expect(callbacks.onExited).not.toHaveBeenCalled();
     jest.runTimersToTime(350);
     expect(callbacks.onExiting).toHaveBeenCalled();
     expect(callbacks.onExited).toHaveBeenCalled();
-
-    wrapper.unmount();
   });
 
   it('should apply correct bootstrap classes', () => {
-    wrapper = mount(<Collapse isOpen={isOpen} />);
-    toggle();
-    expect(wrapper.update().find('div').prop('className')).toBe('collapsing');
-    jest.runTimersToTime(350);
-    expect(wrapper.update().find('div').prop('className')).toBe(
-      'collapse show',
+    const { rerender } = render(
+      <Collapse isOpen={false} data-testid="collapse" />,
     );
-
-    toggle();
-    expect(wrapper.update().find('div').prop('className')).toBe('collapsing');
+    rerender(<Collapse isOpen data-testid="collapse" />);
+    expect(screen.getByTestId('collapse')).toHaveClass('collapsing');
     jest.runTimersToTime(350);
-    expect(wrapper.update().find('div').prop('className')).toBe('collapse');
+    expect(screen.getByTestId('collapse')).toHaveClass('collapse show');
 
-    wrapper.unmount();
+    rerender(<Collapse isOpen={false} data-testid="collapse" />);
+    expect(screen.getByTestId('collapse')).toHaveClass('collapsing');
+    jest.runTimersToTime(350);
+    expect(screen.getByTestId('collapse')).toHaveClass('collapse');
   });
 
-  it('should set inline style to 0 when isOpen change to false', () => {
-    isOpen = true;
-    wrapper = mount(<Collapse isOpen={isOpen} />);
-    toggle();
-    expect(wrapper.state('dimension')).toBe(0);
-    wrapper.unmount();
-  });
-
-  it('should remove inline style when isOpen change to true after transition', () => {
-    wrapper = mount(<Collapse isOpen={isOpen} />);
-    toggle();
+  it('should set inline style to 0 when isOpen change to false and remove after transition', () => {
+    const { rerender } = render(<Collapse isOpen data-testid="collapse" />);
+    rerender(<Collapse isOpen={false} data-testid="collapse" />);
+    expect(screen.getByTestId('collapse').style.height).toBe('0px');
     jest.runTimersToTime(380);
-    expect(wrapper.state('dimension')).toBe(null);
-    wrapper.unmount();
+    expect(screen.getByTestId('collapse').style.height).toBe('');
   });
 });
