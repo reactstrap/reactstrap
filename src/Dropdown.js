f04d3a7976bb8623d7d7453d5195cb1d57f5a6dd
illiteratewriter <manukoshyabraham@gmail.com>
2022-08-26 07:38:57 +0530

    fix(#2467): keyboard support for dropdown listbox (#2584)

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -255,7 +255,7 @@ class Dropdown extends React.Component {
   }
 
   getItemType() {
-    if (this.context.menuRole === 'listbox') {
+    if (this.props.menuRole === 'listbox') {
       return 'option';
     }
     return 'menuitem';
