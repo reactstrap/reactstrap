8a3392023af4a82efe6a565f0f33c9019524cb1b
illiteratewriter <manukoshyabraham@gmail.com>
2022-10-07 08:36:30 +0530

    Migrate to React Testing Library (#2625) (#2626)
    * test(Fade): migrate to rtl
    
    * test(ButtonDropdown): migrate to rtl
    
    * test(Row): migrate to RTL
    
    * test(Spinner): migrate to rtl
    
    * test(Form): migrate to RTL
    
    * test(Form): migrate to RTL
    
    * test(#2625): migrate FormFeedback to rtl

--- src/__tests__/Row.spec.js
+++ src/__tests__/Row.spec.js
@@ -1,43 +1,38 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import { Row } from '..';
+import {
+  testForChildrenInComponent,
+  testForCustomClass,
+  testForDefaultClass,
+} from '../testUtils';
 
 describe('Row', () => {
   it('should render .row markup', () => {
-    const wrapper = shallow(<Row />);
-
-    expect(wrapper.html()).toBe('<div class="row"></div>');
+    testForDefaultClass(Row, 'row');
   });
 
   it('should render children', () => {
-    const wrapper = shallow(<Row>Children</Row>);
-
-    expect(wrapper.html()).toBe('<div class="row">Children</div>');
+    testForChildrenInComponent(Row);
   });
 
   it('should pass additional classNames', () => {
-    const wrapper = shallow(<Row className="extra" />);
-
-    expect(wrapper.hasClass('extra')).toBe(true);
-    expect(wrapper.hasClass('row')).toBe(true);
+    testForCustomClass(Row);
   });
 
   it('show render noGutters class as gx-0', () => {
-    const wrapper = shallow(<Row noGutters />);
-
-    expect(wrapper.hasClass('gx-0')).toBe(true);
-    expect(wrapper.hasClass('row')).toBe(true);
+    render(<Row noGutters data-testid="row" />);
+    expect(screen.getByTestId('row')).toHaveClass('gx-0 row');
   });
 
   it('should pass row col size specific classes as strings', () => {
-    const wrapper = shallow(<Row sm="6" />);
-
-    expect(wrapper.hasClass('row-cols-sm-6')).toBe(true);
+    render(<Row sm="6" data-testid="row" />);
+    expect(screen.getByTestId('row')).toHaveClass('row-cols-sm-6');
   });
 
   it('should pass row col size specific classes as numbers', () => {
-    const wrapper = shallow(<Row sm={6} />);
-
-    expect(wrapper.hasClass('row-cols-sm-6')).toBe(true);
+    render(<Row sm={6} data-testid="row" />);
+    expect(screen.getByTestId('row')).toHaveClass('row-cols-sm-6');
   });
 });
