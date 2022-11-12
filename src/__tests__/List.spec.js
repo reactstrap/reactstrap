683239ced8bcdf1629eb9bd4752f9c3bc620401d
illiteratewriter <manukoshyabraham@gmail.com>
2022-11-12 20:00:17 +0530

    test(List): migrate to RTL

--- src/__tests__/List.spec.js
+++ src/__tests__/List.spec.js
@@ -1,39 +1,34 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import { List } from '..';
+import {
+  testForCustomClass,
+  testForCustomTag,
+  testForDefaultTag,
+} from '../testUtils';
 
 describe('List', () => {
   it('should render with "ul" tag', () => {
-    const wrapper = shallow(<List>Yo!</List>);
-
-    expect(wrapper.type()).toBe('ul');
+    testForDefaultTag(List, 'ul');
   });
 
   it('should render with "list-inline" class when type is "inline"', () => {
-    const wrapper = shallow(<List type="inline">Yo!</List>);
+    render(<List type="inline">Yo!</List>);
 
-    expect(wrapper.text()).toBe('Yo!');
-    expect(wrapper.hasClass('list-inline')).toBe(true);
+    expect(screen.getByText('Yo!')).toHaveClass('list-inline');
   });
 
   it('should render with "list-unstyled" class when type is "unstyled"', () => {
-    const wrapper = shallow(<List type="unstyled">Yo!</List>);
+    render(<List type="unstyled">Yo!</List>);
 
-    expect(wrapper.text()).toBe('Yo!');
-    expect(wrapper.hasClass('list-unstyled')).toBe(true);
+    expect(screen.getByText('Yo!')).toHaveClass('list-unstyled');
   });
 
   it('should render additional classes', () => {
-    const wrapper = shallow(<List className="other">Yo!</List>);
-
-    expect(wrapper.text()).toBe('Yo!');
-    expect(wrapper.hasClass('other')).toBe(true);
+    testForCustomClass(List);
   });
 
   it('should render custom tag', () => {
-    const wrapper = shallow(<List tag="main">Yo!</List>);
-
-    expect(wrapper.text()).toBe('Yo!');
-    expect(wrapper.find('main').length).toBe(1);
+    testForCustomTag(List);
   });
 });
--- src/__tests__/ListInlineItem.spec.js
+++ src/__tests__/ListInlineItem.spec.js
@@ -1,37 +1,25 @@
-import React from 'react';
-import { shallow } from 'enzyme';
 import { ListInlineItem } from '..';
+import {
+  testForChildrenInComponent,
+  testForCustomClass,
+  testForCustomTag,
+  testForDefaultClass,
+} from '../testUtils';
 
 describe('ListInlineItem', () => {
   it('should render children', () => {
-    const listInlineItem = shallow(<ListInlineItem>Yo!</ListInlineItem>).find(
-      'li',
-    );
-    expect(listInlineItem.text()).toBe('Yo!');
+    testForChildrenInComponent(ListInlineItem);
   });
 
   it('should render with "list-inline-item" class', () => {
-    const wrapper = shallow(<ListInlineItem>Yo!</ListInlineItem>);
-
-    expect(wrapper.text()).toBe('Yo!');
-    expect(wrapper.hasClass('list-inline-item')).toBe(true);
+    testForDefaultClass(ListInlineItem, 'list-inline-item');
   });
 
   it('should render additional classes', () => {
-    const wrapper = shallow(
-      <ListInlineItem className="other">Yo!</ListInlineItem>,
-    );
-
-    expect(wrapper.text()).toBe('Yo!');
-    expect(wrapper.hasClass('other')).toBe(true);
-    expect(wrapper.hasClass('list-inline-item')).toBe(true);
+    testForCustomClass(ListInlineItem);
   });
 
   it('should render custom tag', () => {
-    const wrapper = shallow(<ListInlineItem tag="span">Yo!</ListInlineItem>);
-
-    expect(wrapper.text()).toBe('Yo!');
-    expect(wrapper.hasClass('list-inline-item')).toBe(true);
-    expect(wrapper.find('span').length).toBe(1);
+    testForCustomTag(ListInlineItem);
   });
 });
