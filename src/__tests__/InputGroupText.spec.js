4d452a0a650bd03098e37509878237b112f134f2
illiteratewriter <manukoshyabraham@gmail.com>
2022-11-12 09:30:53 +0530

    test(InputGroupText): migrate to RTL

--- src/__tests__/InputGroupText.spec.js
+++ src/__tests__/InputGroupText.spec.js
@@ -1,37 +1,25 @@
-import React from 'react';
-import { shallow } from 'enzyme';
 import { InputGroupText } from '..';
+import {
+  testForCustomClass,
+  testForCustomTag,
+  testForDefaultClass,
+  testForDefaultTag,
+} from '../testUtils';
 
 describe('InputGroupText', () => {
   it('should render with "input-group-text" class', () => {
-    const wrapper = shallow(<InputGroupText>Yo!</InputGroupText>);
-
-    expect(wrapper.text()).toBe('Yo!');
-    expect(wrapper.hasClass('input-group-text')).toBe(true);
+    testForDefaultClass(InputGroupText, 'input-group-text');
   });
 
   it('should render additional classes', () => {
-    const wrapper = shallow(
-      <InputGroupText className="other">Yo!</InputGroupText>,
-    );
-
-    expect(wrapper.hasClass('other')).toBe(true);
-    expect(wrapper.hasClass('input-group-text')).toBe(true);
+    testForCustomClass(InputGroupText);
   });
 
   it('should render with "span" tag by default', () => {
-    const wrapper = shallow(<InputGroupText>Yo!</InputGroupText>);
-
-    expect(wrapper.text()).toBe('Yo!');
-    expect(wrapper.hasClass('input-group-text')).toBe(true);
-    expect(wrapper.find('span').length).toBe(1);
+    testForDefaultTag(InputGroupText, 'span');
   });
 
   it('should render custom tag', () => {
-    const wrapper = shallow(<InputGroupText tag="p">Yo!</InputGroupText>);
-
-    expect(wrapper.text()).toBe('Yo!');
-    expect(wrapper.hasClass('input-group-text')).toBe(true);
-    expect(wrapper.find('p').length).toBe(1);
+    testForCustomTag(InputGroupText);
   });
 });
