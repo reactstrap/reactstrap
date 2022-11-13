aa0fab1f8cc5709fca42e26bdc7421b497953b1e
illiteratewriter <manukoshyabraham@gmail.com>
2022-11-13 23:02:33 +0530

    test(ToastHeader): migrate to RTL

--- src/__tests__/ToastHeader.spec.js
+++ src/__tests__/ToastHeader.spec.js
@@ -1,44 +1,30 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import { ToastHeader } from '..';
+import { testForCustomClass, testForDefaultClass } from '../testUtils';
 
 describe('ToastHeader', () => {
   it('should render with "toast-header" class', () => {
-    const wrapper = shallow(<ToastHeader>Yo!</ToastHeader>);
-
-    expect(wrapper.text()).toBe('Yo!');
-    expect(wrapper.hasClass('toast-header')).toBe(true);
+    testForDefaultClass(ToastHeader, 'toast-header');
   });
 
   it('should render additional classes', () => {
-    const wrapper = shallow(<ToastHeader className="other">Yo!</ToastHeader>);
-
-    expect(wrapper.hasClass('other')).toBe(true);
-    expect(wrapper.hasClass('toast-header')).toBe(true);
+    testForCustomClass(ToastHeader);
   });
 
   it('should render close button', () => {
-    const wrapper = shallow(
-      <ToastHeader toggle={() => {}} className="other">
-        Yo!
-      </ToastHeader>,
-    );
+    render(<ToastHeader toggle={() => {}}>Yo!</ToastHeader>);
 
-    expect(wrapper.hasClass('other')).toBe(true);
-    expect(wrapper.hasClass('toast-header')).toBe(true);
-    expect(wrapper.find('button.btn-close').length).toBe(1);
+    expect(screen.getByLabelText(/close/i)).toBeInTheDocument();
   });
 
   it('should render custom tag', () => {
-    const wrapper = shallow(<ToastHeader tag="p">Yo!</ToastHeader>).childAt(0);
-
-    expect(wrapper.text()).toBe('Yo!');
-    expect(wrapper.type()).toBe('p');
+    render(<ToastHeader tag="p">Yo!</ToastHeader>);
+    expect(screen.getByText(/yo!/i).tagName.toLowerCase()).toMatch('p');
   });
 
   it('should render custom wrapping tag', () => {
-    const wrapper = shallow(<ToastHeader wrapTag="main">Yo!</ToastHeader>);
-
-    expect(wrapper.type()).toBe('main');
+    render(<ToastHeader wrapTag="main">Yo!</ToastHeader>);
+    expect(screen.getByText(/yo/i).parentElement.tagName).toMatch(/main/i);
   });
 });
