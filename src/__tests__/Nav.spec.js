59c5b4b139af787d1e7d73397bb63b2df5d07ae0
Abed <abedabraham98@gmail.com>
2023-02-03 20:09:05 +0000

    Chore (Nav): migrate to RTL

--- src/__tests__/Nav.spec.js
+++ src/__tests__/Nav.spec.js
@@ -1,94 +1,151 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { screen, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import { Nav } from '..';
 
 describe('Nav', () => {
   it('should render .nav markup', () => {
-    const wrapper = shallow(<Nav />);
-
-    expect(wrapper.html()).toBe('<ul class="nav"></ul>');
+    render(
+      <div data-testid="nav">
+        <Nav />
+      </div>,
+    );
+    expect(screen.getByTestId('nav')).toContainHTML('<ul class="nav"></ul>');
   });
 
   it('should render custom tag', () => {
-    const wrapper = shallow(<Nav tag="nav" />);
-
-    expect(wrapper.html()).toBe('<nav class="nav"></nav>');
+    render(
+      <div data-testid="nav">
+        <Nav tag="nav" />
+      </div>,
+    );
+    expect(screen.getByTestId('nav')).toContainHTML('<nav class="nav"></nav>');
   });
 
   it('should render children', () => {
-    const wrapper = shallow(<Nav>Children</Nav>);
-
-    expect(wrapper.html()).toBe('<ul class="nav">Children</ul>');
+    render(
+      <div data-testid="nav">
+        <Nav>Children</Nav>
+      </div>,
+    );
+    expect(screen.getByTestId('nav')).toContainHTML(
+      '<ul class="nav">Children</ul>',
+    );
   });
 
   it('should handle justified prop', () => {
-    const wrapper = shallow(<Nav justified />);
-
-    expect(wrapper.html()).toBe('<ul class="nav nav-justified"></ul>');
+    render(
+      <div data-testid="nav">
+        <Nav justified />
+      </div>,
+    );
+    expect(screen.getByTestId('nav')).toContainHTML(
+      '<ul class="nav nav-justified"></ul>',
+    );
   });
 
   it('should handle fill prop', () => {
-    const wrapper = shallow(<Nav fill />);
-
-    expect(wrapper.html()).toBe('<ul class="nav nav-fill"></ul>');
+    render(
+      <div data-testid="nav">
+        <Nav fill />
+      </div>,
+    );
+    expect(screen.getByTestId('nav')).toContainHTML(
+      '<ul class="nav nav-fill"></ul>',
+    );
   });
 
   it('should handle pills prop', () => {
-    const wrapper = shallow(<Nav pills />);
-
-    expect(wrapper.html()).toBe('<ul class="nav nav-pills"></ul>');
+    render(
+      <div data-testid="nav">
+        <Nav pills />
+      </div>,
+    );
+    expect(screen.getByTestId('nav')).toContainHTML(
+      '<ul class="nav nav-pills"></ul>',
+    );
   });
 
   it('should handle pills prop with card prop', () => {
-    const wrapper = shallow(<Nav pills card />);
-
-    expect(wrapper.html()).toBe(
+    render(
+      <div data-testid="nav">
+        <Nav pills card />
+      </div>,
+    );
+    expect(screen.getByTestId('nav')).toContainHTML(
       '<ul class="nav nav-pills card-header-pills"></ul>',
     );
   });
 
   it('should handle tabs prop', () => {
-    const wrapper = shallow(<Nav tabs />);
-
-    expect(wrapper.html()).toBe('<ul class="nav nav-tabs"></ul>');
+    render(
+      <div data-testid="nav">
+        <Nav tabs />
+      </div>,
+    );
+    expect(screen.getByTestId('nav')).toContainHTML(
+      '<ul class="nav nav-tabs"></ul>',
+    );
   });
 
   it('should handle tabs prop with card prop', () => {
-    const wrapper = shallow(<Nav tabs card />);
-
-    expect(wrapper.html()).toBe(
+    render(
+      <div data-testid="nav">
+        <Nav tabs card />
+      </div>,
+    );
+    expect(screen.getByTestId('nav')).toContainHTML(
       '<ul class="nav nav-tabs card-header-tabs"></ul>',
     );
   });
 
   it('should handle vertical prop', () => {
-    const wrapper = shallow(<Nav vertical />);
-
-    expect(wrapper.html()).toBe('<ul class="nav flex-column"></ul>');
+    render(
+      <div data-testid="nav">
+        <Nav vertical />
+      </div>,
+    );
+    expect(screen.getByTestId('nav')).toContainHTML(
+      '<ul class="nav flex-column"></ul>',
+    );
   });
 
   it('should handle vertical prop with string', () => {
-    const wrapper = shallow(<Nav vertical="sm" />);
-
-    expect(wrapper.html()).toBe('<ul class="nav flex-sm-column"></ul>');
+    render(
+      <div data-testid="nav">
+        <Nav vertical="sm" />
+      </div>,
+    );
+    expect(screen.getByTestId('nav')).toContainHTML(
+      '<ul class="nav flex-sm-column"></ul>',
+    );
   });
 
   it('should handle horizontal prop with string', () => {
-    const wrapper = shallow(<Nav horizontal="end" />);
-
-    expect(wrapper.html()).toBe('<ul class="nav justify-content-end"></ul>');
+    render(
+      <div data-testid="nav">
+        <Nav horizontal="end" />
+      </div>,
+    );
+    expect(screen.getByTestId('nav')).toContainHTML(
+      '<ul class="nav justify-content-end"></ul>',
+    );
   });
 
   it('should pass additional classNames', () => {
-    const wrapper = shallow(<Nav className="extra" />);
-
-    expect(wrapper.hasClass('extra')).toBe(true);
-    expect(wrapper.hasClass('nav')).toBe(true);
+    render(<Nav data-testid="nav" className="extra" />);
+    expect(screen.getByTestId('nav')).toHaveClass('extra');
+    expect(screen.getByTestId('nav')).toHaveClass('nav');
   });
 
   it('should render .navbar-nav class only', () => {
-    const wrapper = shallow(<Nav navbar>Children</Nav>);
-
-    expect(wrapper.html()).toBe('<ul class="navbar-nav">Children</ul>');
+    render(
+      <div data-testid="nav">
+        <Nav navbar>Children</Nav>
+      </div>,
+    );
+    expect(screen.getByTestId('nav')).toContainHTML(
+      '<ul class="navbar-nav">Children</ul>',
+    );
   });
 });
