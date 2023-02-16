9367c2a360be8d660e5ba599334646d3fb819d8e
Abed <abedabraham98@gmail.com>
2023-02-16 17:02:50 +0000

    ran lint and prettier

--- src/__tests__/Nav.spec.js
+++ src/__tests__/Nav.spec.js
@@ -5,129 +5,67 @@ import { Nav } from '..';
 
 describe('Nav', () => {
   it('should render .nav markup', () => {
-    render(
-      <div data-testid="nav">
-        <Nav />
-      </div>,
-    );
-    expect(screen.getByTestId('nav')).toContainHTML('<ul class="nav"></ul>');
+    let { container } = render(<Nav />);
+    expect(container).toContainHTML('<ul class="nav"></ul>');
   });
 
   it('should render custom tag', () => {
-    render(
-      <div data-testid="nav">
-        <Nav tag="nav" />
-      </div>,
-    );
-    expect(screen.getByTestId('nav')).toContainHTML('<nav class="nav"></nav>');
+    let { container } = render(<Nav tag="nav" />);
+    expect(container).toContainHTML('<nav class="nav"></nav>');
   });
 
   it('should render children', () => {
-    render(
-      <div data-testid="nav">
-        <Nav>Children</Nav>
-      </div>,
-    );
-    expect(screen.getByTestId('nav')).toContainHTML(
-      '<ul class="nav">Children</ul>',
-    );
+    let { container } = render(<Nav>Children</Nav>);
+    expect(container).toContainHTML('<ul class="nav">Children</ul>');
   });
 
   it('should handle justified prop', () => {
-    render(
-      <div data-testid="nav">
-        <Nav justified />
-      </div>,
-    );
-    expect(screen.getByTestId('nav')).toContainHTML(
-      '<ul class="nav nav-justified"></ul>',
-    );
+    let { container } = render(<Nav justified />);
+    expect(container).toContainHTML('<ul class="nav nav-justified"></ul>');
   });
 
   it('should handle fill prop', () => {
-    render(
-      <div data-testid="nav">
-        <Nav fill />
-      </div>,
-    );
-    expect(screen.getByTestId('nav')).toContainHTML(
-      '<ul class="nav nav-fill"></ul>',
-    );
+    let { container } = render(<Nav fill />);
+    expect(container).toContainHTML('<ul class="nav nav-fill"></ul>');
   });
 
   it('should handle pills prop', () => {
-    render(
-      <div data-testid="nav">
-        <Nav pills />
-      </div>,
-    );
-    expect(screen.getByTestId('nav')).toContainHTML(
-      '<ul class="nav nav-pills"></ul>',
-    );
+    let { container } = render(<Nav pills />);
+    expect(container).toContainHTML('<ul class="nav nav-pills"></ul>');
   });
 
   it('should handle pills prop with card prop', () => {
-    render(
-      <div data-testid="nav">
-        <Nav pills card />
-      </div>,
-    );
-    expect(screen.getByTestId('nav')).toContainHTML(
+    let { container } = render(<Nav pills card />);
+    expect(container).toContainHTML(
       '<ul class="nav nav-pills card-header-pills"></ul>',
     );
   });
 
   it('should handle tabs prop', () => {
-    render(
-      <div data-testid="nav">
-        <Nav tabs />
-      </div>,
-    );
-    expect(screen.getByTestId('nav')).toContainHTML(
-      '<ul class="nav nav-tabs"></ul>',
-    );
+    let { container } = render(<Nav tabs />);
+    expect(container).toContainHTML('<ul class="nav nav-tabs"></ul>');
   });
 
   it('should handle tabs prop with card prop', () => {
-    render(
-      <div data-testid="nav">
-        <Nav tabs card />
-      </div>,
-    );
-    expect(screen.getByTestId('nav')).toContainHTML(
+    let { container } = render(<Nav tabs card />);
+    expect(container).toContainHTML(
       '<ul class="nav nav-tabs card-header-tabs"></ul>',
     );
   });
 
   it('should handle vertical prop', () => {
-    render(
-      <div data-testid="nav">
-        <Nav vertical />
-      </div>,
-    );
-    expect(screen.getByTestId('nav')).toContainHTML(
-      '<ul class="nav flex-column"></ul>',
-    );
+    let { container } = render(<Nav vertical />);
+    expect(container).toContainHTML('<ul class="nav flex-column"></ul>');
   });
 
   it('should handle vertical prop with string', () => {
-    render(
-      <div data-testid="nav">
-        <Nav vertical="sm" />
-      </div>,
-    );
-    expect(screen.getByTestId('nav')).toContainHTML(
-      '<ul class="nav flex-sm-column"></ul>',
-    );
+    let { container } = render(<Nav vertical="sm" />);
+    expect(container).toContainHTML('<ul class="nav flex-sm-column"></ul>');
   });
 
   it('should handle horizontal prop with string', () => {
-    render(
-      <div data-testid="nav">
-        <Nav horizontal="end" />
-      </div>,
-    );
-    expect(screen.getByTestId('nav')).toContainHTML(
+    let { container } = render(<Nav horizontal="end" />);
+    expect(container).toContainHTML(
       '<ul class="nav justify-content-end"></ul>',
     );
   });
@@ -139,13 +77,7 @@ describe('Nav', () => {
   });
 
   it('should render .navbar-nav class only', () => {
-    render(
-      <div data-testid="nav">
-        <Nav navbar>Children</Nav>
-      </div>,
-    );
-    expect(screen.getByTestId('nav')).toContainHTML(
-      '<ul class="navbar-nav">Children</ul>',
-    );
+    let { container } = render(<Nav navbar>Children</Nav>);
+    expect(container).toContainHTML('<ul class="navbar-nav">Children</ul>');
   });
 });
