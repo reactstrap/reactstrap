af0cf8138e7a384649bc216d37d8ae374171f9f2
Abed <abedabraham98@gmail.com>
2023-03-08 17:43:44 +0000

    Chore (Offcanvas) migrate to RTL -migrate OffcanvasHeader -migrate OffcanvasBody

--- src/__tests__/OffcanvasHeader.spec.js
+++ src/__tests__/OffcanvasHeader.spec.js
@@ -1,50 +1,35 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import { OffcanvasHeader } from '..';
+import { testForCustomClass, testForDefaultClass } from '../testUtils';
 
 describe('OffcanvasHeader', () => {
   it('should render with "offcanvas-header" class', () => {
-    const wrapper = shallow(<OffcanvasHeader>Yo!</OffcanvasHeader>);
-
-    expect(wrapper.text()).toBe('Yo!');
-    expect(wrapper.hasClass('offcanvas-header')).toBe(true);
+    testForDefaultClass(OffcanvasHeader, 'offcanvas-header');
   });
 
   it('should render additional classes', () => {
-    const wrapper = shallow(
-      <OffcanvasHeader className="other">Yo!</OffcanvasHeader>,
-    );
-
-    expect(wrapper.hasClass('other')).toBe(true);
-    expect(wrapper.hasClass('offcanvas-header')).toBe(true);
+    testForCustomClass(OffcanvasHeader);
   });
 
   it('should render close button', () => {
-    const wrapper = shallow(
-      <OffcanvasHeader toggle={() => {}} className="other">
+    render(
+      <OffcanvasHeader toggle={() => {}} data-testid="test" className="other">
         Yo!
       </OffcanvasHeader>,
     );
-
-    expect(wrapper.hasClass('other')).toBe(true);
-    expect(wrapper.hasClass('offcanvas-header')).toBe(true);
-    expect(wrapper.find('button.btn-close').length).toBe(1);
+    const node = screen.getByTestId('test').querySelector('button');
+    expect(node.tagName.toLowerCase()).toBe('button');
+    expect(node).toHaveClass('btn-close');
   });
 
   it('should render custom tag', () => {
-    const wrapper = shallow(
-      <OffcanvasHeader tag="p">Yo!</OffcanvasHeader>,
-    ).childAt(0);
-
-    expect(wrapper.text()).toBe('Yo!');
-    expect(wrapper.type()).toBe('p');
+    render(<OffcanvasHeader tag="h1">Yo!</OffcanvasHeader>);
+    expect(screen.getByText(/yo!/i).tagName.toLowerCase()).toMatch('h1');
   });
 
   it('should render custom wrapping tag', () => {
-    const wrapper = shallow(
-      <OffcanvasHeader wrapTag="main">Yo!</OffcanvasHeader>,
-    );
-
-    expect(wrapper.type()).toBe('main');
+    render(<OffcanvasHeader wrapTag="main">Yo!</OffcanvasHeader>);
+    expect(screen.getByText(/yo/i).parentElement.tagName).toMatch(/main/i);
   });
 });
