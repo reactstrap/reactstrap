451865381e6224fb470b3d06a371e6f672ce26ee
theinfiltrator7 <abedabraham98@gmail.com>
2023-10-04 16:48:27 +0100

    chore: migrate tests to RTL

--- src/__tests__/PopoverHeader.spec.js
+++ src/__tests__/PopoverHeader.spec.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { testForChildrenInComponent } from '../testUtils';
 import { PopoverHeader } from '..';
 
 describe('PopoverHeader', () => {
   it('should render children', () => {
-    const wrapper = shallow(<PopoverHeader>Ello world</PopoverHeader>);
-
-    expect(wrapper.text()).toBe('Ello world');
-    expect(wrapper.hasClass('popover-header')).toBe(true);
+    testForChildrenInComponent(PopoverHeader);
   });
 });
