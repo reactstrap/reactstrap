reactstrap/reactstrap

commit 8457b12b5239dae4c6532598922f18f030954250

Added missing toggle to Accordion

types/lib/Accordion.d.ts
@@ -7,6 +7,7 @@ export interface AccordionProps extends React.HTMLAttributes<HTMLElement> {
   flush?: boolean;
   innerRef?: React.Ref<HTMLElement>;
   open: string | string[];
+  toggle: (targetId: string) => void;
 }
 
 export interface UncontrolledAccordionProps extends Omit<AccordionProps, 'open'> {

commit d58207bc51a9e1ea1f7cf21b4664de30d8d2e3e2

Fixed UncontrolledAccordian typings - regression introduced in 8457b12b5239dae4c6532598922f18f030954250

types/lib/Accordion.d.ts
@@ -10,7 +10,7 @@ export interface AccordionProps extends React.HTMLAttributes<HTMLElement> {
   toggle: (targetId: string) => void;
 }
 
-export interface UncontrolledAccordionProps extends Omit<AccordionProps, 'open'> {
+export interface UncontrolledAccordionProps extends Omit<AccordionProps, 'open' | 'toggle'> {
   defaultOpen?: string | string[];
   stayOpen?: boolean;
 }